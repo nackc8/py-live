8fb3c4d0748027068fb2c175a1fc73adb5922158
Kent Larsson <kent@c8.se>
2025-02-19 15:22:42 +0100

    autocommit complete 1522 (42)

---
+++ date/d250219/for_continue_break.py
@@ -0,0 +1,5 @@
+sum = 0
+for a in range(1, 100):
+    sum = sum + a
+
+print("summa", sum)
--- date/d250219/for_list.py
+++ date/d250219/for_list.py
@@ -1,5 +1,5 @@
-rng = range(10, 20000000000000000000000000000000)  # funkar eftersom ett tal i taget ges
-lst = list(rng)  # funkar inte, då list(..) vill ha alla sifforna i rangen på en gång
+rng = range(10, 20)
+lst = list(rng)
 
 for count in lst:
     print(count)
