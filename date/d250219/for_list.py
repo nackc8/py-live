156dcf9502790a82f67b392437f304f3bc32a141
Kent Larsson <kent@c8.se>
2025-02-19 15:19:13 +0100

    autocommit complete 1519 (13)

--- date/d250219/for_list.py
+++ date/d250219/for_list.py
@@ -1,5 +1,5 @@
-rng = range(10, 20000000000000000000000000000000)
-lst = list(rng)
+rng = range(10, 20000000000000000000000000000000)  # funkar eftersom ett tal i taget ges
+lst = list(rng)  # funkar inte, då list(..) vill ha alla sifforna i rangen på en gång
 
 for count in lst:
     print(count)
