730fa06159f231413c903bf26950774e1caa466f
Kent Larsson <kent@c8.se>
2025-02-19 13:59:38 +0100

    autocommit complete 1359 (38)

---
+++ date/d250219/numbers.py
@@ -0,0 +1,7 @@
+tal1 = 3.14159  # type(tal1) == <class 'float'>
+
+print(type(tal1))
+
+tal2 = tal1
+
+print(tal2)
--- date/d250219/sequence.py
+++ date/d250219/sequence.py
@@ -4,6 +4,11 @@ print(words)
 
 r1 = range(3)  # range skriver ut sig som range(0,3)
 
+print(r1)
+
+# en typ kan konverteras till en annan genom att använda typens
+# namn som en funktion.
+
 r2 = list(range(3))  # en lista skriver ut alla elementen
 
-print(r)
+print(r2)
