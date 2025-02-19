430b0848c23c56e332ec8cc03dc67744f5faccf4
Kent Larsson <kent@c8.se>
2025-02-19 13:55:18 +0100

    autocommit complete 1355 (18)

--- date/d250219/sequence.py
+++ date/d250219/sequence.py
@@ -2,6 +2,8 @@ words = ["apple", "banana", "cherry"]
 
 print(words)
 
-r = list(range(3))
+r1 = range(3)  # range skriver ut sig som range(0,3)
+
+r2 = list(range(3))  # en lista skriver ut alla elementen
 
 print(r)
