4f48af5cd4b25249f7f822273f29995034db955a
Kent Larsson <kent@c8.se>
2025-02-19 14:45:04 +0100

    autocommit complete 1445 (04)

--- date/d250219/conditionals.py
+++ date/d250219/conditionals.py
@@ -3,7 +3,7 @@ age = int(input("Hur gammal är du? "))
 if age >= 18:
     print("Du får köra bil!")
     print("Nää nää nääänääää")
-elif age >= 16:
+elieif age >= 16:
     print("Du får övningsköra!")
 else:
     print("Du får inte köra bil!")
