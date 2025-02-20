c53ef4a9279e1a7fa667dd42db0cf0fee09312de
Kent Larsson <kent@c8.se>
2025-02-20 09:59:09 +0100

    autocommit complete 0959 (09)

--- date/d250220/more_lists.py
+++ date/d250220/more_lists.py
@@ -2,7 +2,7 @@ numbers = [1, 2, 3, 4, 5]
 
 pos_3_in_numbers = numbers[3]
 
-print(numbers[0])
+print(numbers[0])  # prints 1
 
 # TODO: Vi gör en egen funktion som kan ta ut flera index på en gång!
 pos_1_in_numbers = numbers[1]
@@ -15,6 +15,8 @@ tic_tac_toe = [[" ", "X", " "], ["O", "X", "O"], [" ", " ", "X"]]
 
 # if sats som kollar om X har vunnit diagonalt från högst upp
 # till vänster, till längst ned till höger
+
+# Ej vunnit
 
 if tic_tac_toe[0][0] == "X" and tic_tac_toe[1][1] == "X" and tic_tac_toe[2][2] == "X":
     print("X vann diagonalt från övre vänstra hörnet, till nedre högra")
