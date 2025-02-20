4a2eb5558f1839969a931dd7c33950b07ddc9e4a
Kent Larsson <kent@c8.se>
2025-02-20 13:15:18 +0100

    autocommit complete 1315 (18)

--- date/d250220/for_for.py
+++ date/d250220/for_for.py
@@ -2,5 +2,5 @@
 
 for tabell in range(1, 10):
     print("## Multiplikationstabell", tabell)
-    for nummer in range(1, tabell):
+    for nummer in range(1, tabell + 1):
         print(tabell, "*", nummer)
