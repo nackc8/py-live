312e73a9461a3184a2a21297e5ad29f5460feb9a
Kent Larsson <kent@c8.se>
2025-02-20 13:40:23 +0100

    autocommit complete 1340 (23)

--- date/d250220/for_continue.py
+++ date/d250220/for_continue.py
@@ -1,10 +1,10 @@
 # Skriv ut alla jämna tal
 # for num in range(1,10):
 
-print(5 % 1)
-print(5 % 2)
+print(5 % 1, "<- 0")
+print(5 % 2, "<- (5 - 2*2 ) = 1")
 print(5 % 3, "<- 2")
 print(5 % 4)
 print(5 % 5)
 
-print(1 % 5)
+print(1 % 5, "<- (1 - 0*5) = 1")
