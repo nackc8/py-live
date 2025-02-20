6aeec35d97d72eabbb50e8073b3f47010139d6c0
Kent Larsson <kent@c8.se>
2025-02-20 11:21:46 +0100

    autocommit complete 1121 (46)

--- date/d250220/while_true.py
+++ date/d250220/while_true.py
@@ -6,7 +6,7 @@ while True:
     num = input("Skriv ett heltal eller exit: ")
     if num.isdigit():
         sum += int(num)
-    elif num == "exit":
+    elif num.capitalize() == "Exit":
         break
     else:
         print("Du skrev varken en siffra eller exit!")
