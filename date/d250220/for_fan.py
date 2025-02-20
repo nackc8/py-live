69f67f2bef261f24c79087b14e4210598ae40b31
Kent Larsson <kent@c8.se>
2025-02-20 12:57:15 +0100

    autocommit complete 1257 (15)

--- date/d250220/for_fan.py
+++ date/d250220/for_fan.py
@@ -1,4 +1,9 @@
 fan_orders = [3, 10, 120, 73, 94, 56, 83]
 fan_orders_plus_small_order_fee = []
 
-for
+small_order_fee_limit = 50
+small_order_fee_percent = 1.2
+
+for banan in fan_orders:
+    if banan < small_order_fee_limit:
+        banan = banan * small_order_fee_percent
