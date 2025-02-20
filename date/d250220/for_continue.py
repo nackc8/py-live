472de07016fdaee2dd629cfcc4cd7f5f354e879a
Kent Larsson <kent@c8.se>
2025-02-20 13:36:48 +0100

    autocommit complete 1336 (48)

--- date/d250220/for_continue.py
+++ date/d250220/for_continue.py
@@ -1,8 +1,10 @@
 # Skriv ut alla jämna tal
 # for num in range(1,10):
 
-print(1 % 5)
-print(2 % 5)
-print(3 % 5)
-print(4 % 5)
+print(5 % 1)
+print(5 % 2)
+print(5 % 3)
+print(5 % 4)
 print(5 % 5)
+
+print(1 % 5)
