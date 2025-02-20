99bdaf313d707ed48545a3acdec2d8e012ce26b1
Kent Larsson <kent@c8.se>
2025-02-20 10:38:15 +0100

    autocommit complete 1038 (15)

--- date/d250220/inputs.py
+++ date/d250220/inputs.py
@@ -2,9 +2,9 @@ standard_input = "gUL"  # för "kattens" skull
 
 col = input("Skriv din favoritfärg: ")
 
-col = col.capitalize()
+col = col.capitalize()  # "normalisera" texten i variabeln
 
-is_ugly = col == "gul"
+is_ugly = col == "Gul"
 
 if is_ugly:
     print("gult är fult!")
