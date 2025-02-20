79878232a13247d1271124e61891a8716f25bf25
Kent Larsson <kent@c8.se>
2025-02-20 12:43:47 +0100

    autocommit complete 1243 (47)

--- date/d250220/for.py
+++ date/d250220/for.py
@@ -1,2 +1,16 @@
-for num in range(1, 10):
+# for num in range(1, 10):
+#     print(num)
+# # matar ut
+# # 1
+# # 2
+# # 3
+# # 4
+# # 5
+# # 6
+# # 7
+# # 8
+# # 9
+
+num = 1
+while num < 10:
     print(num)
