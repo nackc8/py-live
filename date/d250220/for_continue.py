7f2df65cab1cf9bc096a8f66dd82330455bc7dad
Kent Larsson <kent@c8.se>
2025-02-20 13:44:51 +0100

    autocommit complete 1344 (51)

--- date/d250220/for_continue.py
+++ date/d250220/for_continue.py
@@ -1,10 +1,25 @@
 # Skriv ut alla jämna tal
 # for num in range(1,10):
 
-print(5 % 1, "<- 0")
-print(5 % 2, "<- (5 - 2*2 ) = 1")
-print(5 % 3, "<- 2")
-print(5 % 4)
-print(5 % 5)
+# print(5 % 1, "<- 0")
+# print(5 % 2, "<- (5 - 2*2 ) = 1")
+# print(5 % 3, "<- 2")
+# print(5 % 4)
+# print(5 % 5)
 
-print(1 % 5, "<- (1 - 0*5) = 1")
+# print(1 % 5, "<- (1 - 0*5) = 1")
+
+# # Utan modulo
+# num = 0
+# for _ in range(300):
+#     if num == 50:
+#         num = 0
+
+#     print(num)
+#     num += 1
+
+# Utan modulo
+num = 0
+for _ in range(300):
+    print(num)
+    num = (num + 1) % 50
