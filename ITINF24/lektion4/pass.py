d268f16fab2b4612e4cd56f1d6e3ae2b033004b9
Kent Larsson <kent@c8.se>
2025-02-24 13:01:41 +0100

    autocommit complete 1301 (41)

--- ITINF24/lektion4/pass.py
+++ ITINF24/lektion4/pass.py
@@ -1,2 +1,10 @@
-while True:
-    pass
+x = 123
+
+if x < -50:
+    pass  # väntar med detta "fall"
+elif x > 100:
+    pass  # väntar med detta "fall"
+else:
+    print("hello world")
+    for x in ["a", "b"]:
+        pass  # skriver vad jag ska göra sen
