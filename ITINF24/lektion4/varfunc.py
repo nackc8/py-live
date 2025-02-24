21dc70a752e70f5c67b54018eb6e668aa23a0e05
Kent Larsson <kent@c8.se>
2025-02-24 13:08:16 +0100

    autocommit complete 1308 (16)

--- ITINF24/lektion4/varfunc.py
+++ ITINF24/lektion4/varfunc.py
@@ -1,7 +1,13 @@
+def halvera(tal):
+    return tal / 2
+
+
 def dubbla(tal):
     return tal * 2
 
-hehe = dubbla
 
+# hehe pekar på samma
+# funktion som dubbla
+hehe = dubbla
 a = dubbla(10)
 b = hehe(10)
