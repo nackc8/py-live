837b8215ddba8cb26608483e16308b090a57308c
Kent Larsson <kent@c8.se>
2025-02-24 10:55:49 +0100

    autocommit complete 1055 (49)

--- ITINF24/lektion3/myutil1_user.py
+++ ITINF24/lektion3/myutil1_user.py
@@ -3,6 +3,6 @@ import myutil1
 min_nya_lista = ["Björn", "Älg", "Myrslok"]
 
 result = myutil1.get_first_second(min_nya_lista)
-print("myutil1.min_lista", myutil1.FAKE_PI)
+print("Bättre PI", myutil1.FAKE_PI)
 
 print("Resultat", result)
