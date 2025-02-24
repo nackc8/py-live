02bea3bcbd12a32b27a22743a8c5a7f951bd7940
Kent Larsson <kent@c8.se>
2025-02-24 13:59:25 +0100

    autocommit complete 1359 (25)

--- ITINF24/lektion4/func_named4.py
+++ ITINF24/lektion4/func_named4.py
@@ -9,5 +9,9 @@ def myprint(txt, /, *, start="ZZZ"):
     print(start + txt)
 
 
+def myprint2(txt1, /, txt2, *, start="ZZZ"):
+    print(start + txt1 + txt2)
+
+
 myprint("Hello world", start="Inte zeta")
 myprint(txt="Hello world", start="Inte zeta")
