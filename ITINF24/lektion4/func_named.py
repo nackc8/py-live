dd3c16897cd647ece69c8d415f887fd3fd38ee27
Kent Larsson <kent@c8.se>
2025-02-24 13:45:22 +0100

    autocommit complete 1345 (22)

--- ITINF24/lektion4/func_named.py
+++ ITINF24/lektion4/func_named.py
@@ -2,6 +2,6 @@ def dela(a, b):
     return a / b
 
 
-# samma! 
+# samma! Funktioner def:ade enligt ovan kan anropas med positionella argument eller namngivna
 print("Rad", "1", dela(5, 2), sep=": ")
-print("Rad", "2", dela(b=2, a=5), sep="\n   ")
+print("Rad:\n------", "2", dela(b=2, a=5), sep="\n   ", end="\n------", )
