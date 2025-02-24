0f61ec754842ddf5942de3d0e1c79ac2d1842424
Kent Larsson <kent@c8.se>
2025-02-24 12:56:19 +0100

    autocommit complete 1256 (19)

--- ITINF24/lektion4/utf8.py
+++ ITINF24/lektion4/utf8.py
@@ -4,7 +4,4 @@ print("Du fick: 🃁")
 r = range(1000, 1300)
 print(r[0])  # -> 1000 eftersom det är först (0)
 
-alfa = ["a", "b", "c"]
-alfa_range = range(0, 3)
-for index in alfa_range:
-    alfa[index]
+abs_three = abs(-3)
