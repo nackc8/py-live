c4dc873e887127eb47ff145c6f7155299867bf7c
Kent Larsson <kent@c8.se>
2025-02-25 10:37:29 +0100

    autocommit complete 1037 (29)

--- ITINF24/lektion5/slicing5.py
+++ ITINF24/lektion5/slicing5.py
@@ -3,25 +3,6 @@
 lst = ["a", "b", "c", "d", "e", "f"]
 by = "skinnskatteberg"
 
-# läsning av delar
+print(lst[::2])  # Varannat element
 
-# kombinera slicing 3 och 4.
-
-print(lst[1:5])  # omsträngens längd är exakt som ovan
-
-# mer generellt
-print(lst[1:-1])
-print(by[1:-1])
-
-
-# tre näst sista
-print(lst[-4:-1])
-print(by[-4:-1])
-
-# tvärtom test - det kraschar inte!
-print("tvärtom", lst[-1:-4])
-print("tvärtom", by[-1:-4])
-
-
-# print(lst[::2])  # Varannat element
 # print(lst[::-1])  # Omvänd lista
