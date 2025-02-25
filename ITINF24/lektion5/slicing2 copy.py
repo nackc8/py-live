cd9816e834e7c3a9ec9c08d70c0b7920c7f6bd4d
Kent Larsson <kent@c8.se>
2025-02-25 10:24:12 +0100

    autocommit partial 1024 (12)

--- ITINF24/lektion5/slicing2 copy.py
+++ ITINF24/lektion5/slicing2 copy.py
@@ -5,13 +5,6 @@ by = "skinnskatteberg"
 
 # läsning av delar
 
-print(lst[2:])  # Från och med index 2 till slutet
-print(lst[-3:])  # Från tredje sista till slutet
-
-print(by[2:])  # Från och med index 2 till slutet
-print(by[-3:])  # Från tredje sista till slutet
-
-
 # print(lst[:4])  # Från början till (ej inklusive) index 4
 # print(lst[::2])  # Varannat element
 # print(lst[::-1])  # Omvänd lista
