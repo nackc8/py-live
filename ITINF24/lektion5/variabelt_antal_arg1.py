52e5bc0346107179cec58c0ead5946b03a1924f1
Kent Larsson <kent@c8.se>
2025-02-25 11:08:37 +0100

    autocommit complete 1108 (37)

--- ITINF24/lektion5/variabelt_antal_arg1.py
+++ ITINF24/lektion5/variabelt_antal_arg1.py
@@ -1,8 +1,9 @@
 import random
 
+
 #        .---------- * först gör att man kan få in en sekvens av positionella argument
 def xuff(*params):
-    print("positionella argument:")
+    print(f"{random.randint(1, 999)} positionella argument:")
     for p in params:
         print("\t", p)
 
