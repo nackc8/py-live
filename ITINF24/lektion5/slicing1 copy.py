015a29cd2dbce50eecd5e8f14f471801c19b55a3
Kent Larsson <kent@c8.se>
2025-02-25 10:13:14 +0100

    autocommit partial 1013 (14)

---
+++ ITINF24/lektion5/slicing1 copy.py
@@ -0,0 +1,14 @@
+# slicing fungerar för alla sekvenser
+
+lst = ["a", "b", "c", "d", "e", "f"]
+print(lst[0])  # Första elementet
+# 3 och ändra icke negativa tal hittar element från
+# början till slutet.
+print(lst[-1])  # Sista elementet
+print(lst[-6])  # Första elementet, räknat från slutet
+
+
+# print(lst[2:])  # Från index 2 till slutet
+# print(lst[:4])  # Från början till (ej inklusive) index 4
+# print(lst[::2])  # Varannat element
+# print(lst[::-1])  # Omvänd lista
