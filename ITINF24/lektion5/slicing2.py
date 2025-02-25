67904883da1514a09480f128d8086555c317d17f
Kent Larsson <kent@c8.se>
2025-02-25 10:21:04 +0100

    autocommit complete 1021 (04)

--- ITINF24/lektion5/slicing2.py
+++ ITINF24/lektion5/slicing2.py
@@ -1,7 +1,11 @@
 # slicing fungerar för alla sekvenser
 
+lst = ["a", "b", "c", "d", "e", "f"]
 
-# print(lst[2:])  # Från index 2 till slutet
+# läsning av delar
+
+print(lst[2:])  # Från index 2 till slutet
+
 # print(lst[:4])  # Från början till (ej inklusive) index 4
 # print(lst[::2])  # Varannat element
 # print(lst[::-1])  # Omvänd lista
