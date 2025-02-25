898c89d8919537e2b90095affa0cdad278dd5bf9
Kent Larsson <kent@c8.se>
2025-02-25 10:13:30 +0100

    autocommit partial 1013 (30)

---
+++ ITINF24/lektion5/slicing2.py
@@ -0,0 +1,7 @@
+# slicing fungerar för alla sekvenser
+
+
+# print(lst[2:])  # Från index 2 till slutet
+# print(lst[:4])  # Från början till (ej inklusive) index 4
+# print(lst[::2])  # Varannat element
+# print(lst[::-1])  # Omvänd lista
