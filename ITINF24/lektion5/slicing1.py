a18afb298f0ea15a96abbc08463aeb6e438d34b0
Kent Larsson <kent@c8.se>
2025-02-25 09:56:48 +0100

    autocommit complete 0956 (48)

--- ITINF24/lektion5/slicing1.py
+++ ITINF24/lektion5/slicing1.py
@@ -2,7 +2,10 @@
 
 lst = [0, 1, 2, 3, 4, 5]
 print(lst[0])  # Första elementet
-print(lst[2:])  # Från index 2 till slutet
-print(lst[:4])  # Från början till (ej inklusive) index 4
-print(lst[::2])  # Varannat element
-print(lst[::-1])  # Omvänd lista
+
+
+
+# print(lst[2:])  # Från index 2 till slutet
+# print(lst[:4])  # Från början till (ej inklusive) index 4
+# print(lst[::2])  # Varannat element
+# print(lst[::-1])  # Omvänd lista
