fa27ff2f57eb3a9ac9fb3884eaf724401de47e37
Kent Larsson <kent@c8.se>
2025-02-25 09:45:04 +0100

    autocommit complete 0945 (04)

---
+++ ITINF24/lektion5/slicing1.py
@@ -0,0 +1,7 @@
+# slicing fungerar för alla sekvenser
+# fast tilldelning via slice fungerar bara på "muterbara" sekvenser
+
+# mutable vs immutable
+# mutable: kan ändras
+# immutable: ändras aldrig
+
