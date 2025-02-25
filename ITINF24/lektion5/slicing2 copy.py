e4936508ba78d38b2372503d3626f28033e21c97
Kent Larsson <kent@c8.se>
2025-02-25 10:23:55 +0100

    autocommit partial 1023 (55)

--- ITINF24/lektion5/slicing2 copy.py
+++ ITINF24/lektion5/slicing2 copy.py
@@ -10,3 +10,8 @@ print(lst[-3:])  # Från tredje sista till slutet
 
 print(by[2:])  # Från och med index 2 till slutet
 print(by[-3:])  # Från tredje sista till slutet
+
+
+# print(lst[:4])  # Från början till (ej inklusive) index 4
+# print(lst[::2])  # Varannat element
+# print(lst[::-1])  # Omvänd lista
