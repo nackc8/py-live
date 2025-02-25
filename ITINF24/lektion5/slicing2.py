5d79d1f9e91a5836f673e300b3d43100e82a03ac
Kent Larsson <kent@c8.se>
2025-02-25 10:23:38 +0100

    autocommit complete 1023 (38)

--- ITINF24/lektion5/slicing2.py
+++ ITINF24/lektion5/slicing2.py
@@ -1,10 +1,15 @@
 # slicing fungerar för alla sekvenser
 
 lst = ["a", "b", "c", "d", "e", "f"]
+by = "skinnskatteberg"
 
 # läsning av delar
 
-print(lst[-1:])  # Från och med index 2 till slutet
+print(lst[2:])  # Från och med index 2 till slutet
+print(lst[-3:])  # Från tredje sista till slutet
+
+print(by[2:])  # Från och med index 2 till slutet
+print(by[-3:])  # Från tredje sista till slutet
 
 
 # print(lst[:4])  # Från början till (ej inklusive) index 4
