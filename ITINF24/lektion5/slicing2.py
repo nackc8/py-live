a68f4e71197152d9c71e119e652bed63e58bb856
Kent Larsson <kent@c8.se>
2025-02-25 10:22:26 +0100

    autocommit complete 1022 (26)

--- ITINF24/lektion5/slicing2.py
+++ ITINF24/lektion5/slicing2.py
@@ -4,7 +4,8 @@ lst = ["a", "b", "c", "d", "e", "f"]
 
 # läsning av delar
 
-print(lst[2:])  # Från index 2 till slutet
+print(lst[-1:])  # Från och med index 2 till slutet
+
 
 # print(lst[:4])  # Från början till (ej inklusive) index 4
 # print(lst[::2])  # Varannat element
