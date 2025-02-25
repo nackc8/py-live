31817694992b7cdd4a75864f409dbc4013b4b39c
Kent Larsson <kent@c8.se>
2025-02-25 09:59:30 +0100

    autocommit complete 0959 (30)

--- ITINF24/lektion5/slicing1.py
+++ ITINF24/lektion5/slicing1.py
@@ -1,10 +1,11 @@
 # slicing fungerar för alla sekvenser
 
 lst = ["a", "b", "c", "d", "e", "f"]
-print(lst[3])  # Första elementet
+print(lst[0])  # Första elementet
 # 3 och ändra icke negativa tal hittar element från
 # början till slutet.
 print(lst[-1])  # Sista elementet
+print(lst[-6])  # Sista elementet
 
 
 # print(lst[2:])  # Från index 2 till slutet
