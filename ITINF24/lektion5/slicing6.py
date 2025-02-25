706d37ee8791025381d1d17de686a05837e86c77
Kent Larsson <kent@c8.se>
2025-02-25 10:52:23 +0100

    autocommit complete 1052 (23)

--- ITINF24/lektion5/slicing6.py
+++ ITINF24/lektion5/slicing6.py
@@ -3,17 +3,14 @@
 lst = ["a", "b", "c", "d", "e", "f"]
 by = "skinnskatteberg"
 
+print(lst[::-1])  # Omvänd, baklänges
+print(by[::-1])  # Omvänd, baklänges
 
-# print(lst[::-1])  # Omvänd, baklänges
-# print(by[::-1])  # Omvänd, baklänges
+print(lst[2::-1])  # Börjar vid index 2
+print(by[2::-1])  # Börjar vid index 2
 
-
-# print(lst[2::-1])  # Börjar vid index 2
-# print(by[2::-1])  # Börjar vid index 2
-
-# print(lst[4:1:-1])  # Börjar vid index 4, slutar vid index 1 (ej inklusivt)
-# print(by[4:1:-1])  # Börjar vid index 4, slutar vid index 1 (ej inklusivt)
-
+print(lst[4:1:-1])  # Börjar vid index 4, slutar vid index 1 (ej inklusivt)
+print(by[4:1:-1])  # Börjar vid index 4, slutar vid index 1 (ej inklusivt)
 
 print(lst[4::-1])  # Börjar vid index 4, slutar vid första elementet
 print(by[4::-1])  # Börjar vid index 4, slutar vid första elementet
