4823827e12048477cadf855a9f0a792054eb9a28
Kent Larsson <kent@c8.se>
2025-02-25 09:27:13 +0100

    autocommit complete 0927 (13)

---
+++ ITINF24/lektion5/sequences.py
@@ -0,0 +1,11 @@
+# listor är sekvenser
+lst = ["hund", "katt", "oxe"]
+
+# strängar är sekvenser
+djur = "myrslok"
+
+# range()es är sekvenser
+nummer = range(1000, 1100, 100)
+
+# sekvenser kan ge tillbaka
+# elementet på ett visst index
