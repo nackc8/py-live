72611e54b5354c70b978448751f6ffdaa2b71bd0
Kent Larsson <kent@c8.se>
2025-02-25 11:19:01 +0100

    autocommit complete 1119 (01)

--- ITINF24/lektion5/variabelt_antal_arg2.py
+++ ITINF24/lektion5/variabelt_antal_arg2.py
@@ -3,6 +3,7 @@ import random
 
 #                   .---------- * först gör att man kan få in en sekvens av positionella argument
 def tal_delare(tal, *params):
+    """"""
     print(f"{random.randint(1, 999)} tal_delare:")
     for p in params:
         print("", "loop-snurr, p=", p)
