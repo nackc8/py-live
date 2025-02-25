db7a9d605288e1ad8ac8e4ffed1c187b2b236803
Kent Larsson <kent@c8.se>
2025-02-25 09:31:41 +0100

    autocommit complete 0931 (41)

--- ITINF24/lektion5/sequences.py
+++ ITINF24/lektion5/sequences.py
@@ -1,8 +1,10 @@
 # listor är sekvenser
 lst = ["hund", "katt", "oxe"]
+#              ^ index 1
 
 # strängar är sekvenser
 djur = "myrslok"
+#        ^ index 1
 
 # range()es är sekvenser
 nummer = range(1000, 1301, 100)
@@ -11,3 +13,5 @@ nummer = range(1000, 1301, 100)
 # elementet på ett visst index
 
 second_number = nummer[1]
+second_item = lst[1]
+second_char = djur[1]
