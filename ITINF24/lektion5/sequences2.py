c0e73886a6a7b8e9bdafa89a402ba09b31cbf4ab
Kent Larsson <kent@c8.se>
2025-02-25 09:39:49 +0100

    autocommit complete 0939 (49)

--- ITINF24/lektion5/sequences2.py
+++ ITINF24/lektion5/sequences2.py
@@ -8,3 +8,11 @@ for bokstav in djur:
         print(bokstav, end="")
 
     tecken += 1
+print()
+
+
+# https://docs.python.org/3.11/library/stdtypes.html#sequence-types-list-tuple-range
+# relevant:
+#   x in s
+if "s" in djur:
+    print("s-djur!")
