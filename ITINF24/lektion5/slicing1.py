873f3b1ff9c2f8fce670cfff327c98985dff739f
Kent Larsson <kent@c8.se>
2025-02-25 09:56:16 +0100

    autocommit complete 0956 (16)

--- ITINF24/lektion5/slicing1.py
+++ ITINF24/lektion5/slicing1.py
@@ -1,2 +1,8 @@
 # slicing fungerar för alla sekvenser
 
+lst = [0, 1, 2, 3, 4, 5]
+print(lst[0])  # Första elementet
+print(lst[2:])  # Från index 2 till slutet
+print(lst[:4])  # Från början till (ej inklusive) index 4
+print(lst[::2])  # Varannat element
+print(lst[::-1])  # Omvänd lista
