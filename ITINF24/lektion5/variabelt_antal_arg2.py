d7f4d488464261c3e96448d6821310bc126a2812
Kent Larsson <kent@c8.se>
2025-02-25 11:17:22 +0100

    autocommit complete 1117 (22)

--- ITINF24/lektion5/variabelt_antal_arg2.py
+++ ITINF24/lektion5/variabelt_antal_arg2.py
@@ -3,12 +3,13 @@ import random
 
 #                   .---------- * först gör att man kan få in en sekvens av positionella argument
 def tal_delare(tal, *params):
-    print(f"{random.randint(1, 999)} positionella argument:")
+    print(f"{random.randint(1, 999)} tal_delare:")
     for p in params:
+        print("", "loop-snurr, p=", p)
         tal = tal / p
-        print(tal)
     return tal
 
 
-behandlat_tal = tal_delare(1000)
+# behandlat_tal = tal_delare(1000)
 # behandlat_tal2 = tal_delare(1000, 2)
+behandlat_tal3 = tal_delare(1000, 2, 0.5)
