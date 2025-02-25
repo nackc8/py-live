953408ff21a7cf538a679afc60f7fd4f1e1669f3
Kent Larsson <kent@c8.se>
2025-02-25 10:41:10 +0100

    autocommit complete 1041 (10)

--- ITINF24/lektion5/slicing5.py
+++ ITINF24/lektion5/slicing5.py
@@ -6,8 +6,11 @@ by = "skinnskatteberg"
 # print(lst[::2])  # Varannat element
 # print(by[::2])  # Varannat element
 
-print(lst[1::2])  # Varannat element från andra elementet
-print(by[1::2])  # Varannat element från andra elementet
+# print(lst[1::2])  # Varannat element från andra elementet
+# print(by[1::2])  # Varannat element från andra elementet
+
+lst = ["a", "b", "c", "d", "e", "f"]
+by = "skinnskatteberg"
 
 print(lst[1:3:2])  # Varannat element från andra elementet
 print(by[1:3:2])  # Varannat element från andra elementet
