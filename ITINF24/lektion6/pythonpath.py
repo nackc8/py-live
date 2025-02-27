bb4d3fcc6e5914caa0efa2297b2b1a0952e7bb15
Kent Larsson <kent@c8.se>
2025-02-27 11:17:40 +0100

    autocommit complete 1117 (40)

--- ITINF24/lektion6/pythonpath.py
+++ ITINF24/lektion6/pythonpath.py
@@ -13,3 +13,6 @@ utan_påverkan = [
     "/usr/lib/python3/dist-packages",
     "/usr/lib/python3.11/dist-packages",
 ]
+
+# med följande PYTHONPATH exporterad:
+#   export 'PYTHONPATH=/shared/kent/Documents/nackademin/python/py-live/ITINF24/lektion1'
