f9172e190331041af4581474cda7b4f815a75c06
Kent Larsson <kent@c8.se>
2025-02-27 10:28:27 +0100

    autocommit complete 1028 (27)

--- ITINF24/lektion6/py-palindromish-exempel/uppgift.py
+++ ITINF24/lektion6/py-palindromish-exempel/uppgift.py
@@ -2,21 +2,11 @@ def palindromish(s, g=None):
     pass
 
 
-print(
-    "palindromish('radar', 2) == True",
-    palindromish("radar", 2) == True,
-    "🐛" if palindromish("radar", 2) == True else "",
-)
-
-
-##
-#
-#
-#
-# palindromish([1, 2, 3, 50, 60, 3, 2, 1], 3) == True
-# palindromish([1, 2, 3, 50, 60, 3, 2, 1], 4) == False
-# palindromish("example", 3) == False
-# palindromish("naturrutan") == True
-# palindromish("kajak", 1000) == True
-# palindromish("solros", 2) == True
-# palindromish("solros", 3) == False
+print(palindromish("radar", 2) == True)
+print(palindromish([1, 2, 3, 50, 60, 3, 2, 1], 3) == True)
+print(palindromish([1, 2, 3, 50, 60, 3, 2, 1], 4) == False)
+print(palindromish("example", 3) == False)
+print(palindromish("naturrutan") == True)
+print(palindromish("kajak", 1000) == True)
+print(palindromish("solros", 2) == True)
+print(palindromish("solros", 3) == False)
