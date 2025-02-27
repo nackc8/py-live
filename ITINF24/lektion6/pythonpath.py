00f6affd153d719ed76551e20a21d191c2e3763a
Kent Larsson <kent@c8.se>
2025-02-27 11:14:34 +0100

    autocommit complete 1114 (34)

--- ITINF24/lektion6/pythonpath.py
+++ ITINF24/lektion6/pythonpath.py
@@ -4,3 +4,12 @@ import sys
 print("")
 print("")
 print("sys.path", sys.path)
+x = [
+    "/shared/kent/Documents/nackademin/python/py-live/ITINF24/lektion6",
+    "/usr/lib/python311.zip",
+    "/usr/lib/python3.11",
+    "/usr/lib/python3.11/lib-dynload",
+    "/usr/local/lib/python3.11/dist-packages",
+    "/usr/lib/python3/dist-packages",
+    "/usr/lib/python3.11/dist-packages",
+]
