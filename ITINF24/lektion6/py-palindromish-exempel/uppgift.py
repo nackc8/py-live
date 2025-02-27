3606552bec9279c18da97cf20d68fef111a2db0d
Kent Larsson <kent@c8.se>
2025-02-27 10:23:12 +0100

    autocommit complete 1023 (12)

--- ITINF24/lektion6/py-palindromish-exempel/uppgift.py
+++ ITINF24/lektion6/py-palindromish-exempel/uppgift.py
@@ -1,4 +1,12 @@
-palindromish(s, g=None):
+def palindromish(s, g=None):
     pass
 
-palindromish("radar", 2)
+
+palindromish("radar", 2) == True
+palindromish([1, 2, 3, 50, 60, 3, 2, 1], 3) == True
+palindromish([1, 2, 3, 50, 60, 3, 2, 1], 4) == False
+palindromish("example", 3) == False
+palindromish("naturrutan") == True
+palindromish("kajak", 1000) == True
+palindromish("solros", 2) == True
+palindromish("solros", 3) == False
