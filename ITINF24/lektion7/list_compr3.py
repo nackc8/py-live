bfa636dd4e636bea6dde79291450a5fb7212675a
Kent Larsson <kent@c8.se>
2025-03-03 10:45:57 +0100

    autocommit partial 1045 (57)

--- ITINF24/lektion7/list_compr3.py
+++ ITINF24/lektion7/list_compr3.py
@@ -1,8 +1,4 @@
-lst1 = []
-for n in range(0, 10):
-    val = 2**n + n
-    if val % 2 == 0:
-        lst1.append(val)
+ord = [""]
 
 # Krångligare?
 lst2 = [2**n + n for n in range(0, 10) if (2**n + n) % 2 == 0]
