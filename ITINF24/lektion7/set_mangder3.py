5ef84207d401080e5f268bd1068d0ba8a58571af
Kent Larsson <kent@c8.se>
2025-03-03 12:57:47 +0100

    autocommit complete 1257 (47)

--- ITINF24/lektion7/set_mangder3.py
+++ ITINF24/lektion7/set_mangder3.py
@@ -1,5 +1,7 @@
 p = print
 
 words = {"burk", "bil", "kanot", "rep"}
-words2 = {"burk", "spis", "hamburgare"}
+words2 = {"burk"}
 words3 = {"röd", "blå"}
+
+p("words2 issubset i words?", words2.issubset(words))
