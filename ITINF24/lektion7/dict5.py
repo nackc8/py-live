869c8b90d149946b6857c8216433fcb47bb1b3b6
Kent Larsson <kent@c8.se>
2025-03-03 13:55:20 +0100

    autocommit complete 1355 (20)

--- ITINF24/lektion7/dict5.py
+++ ITINF24/lektion7/dict5.py
@@ -6,17 +6,8 @@ ppl = {
     "4502129999": {"firstname": "Torleif", "surname": "Bengtsson"},
     "1501019999": {"firstname": "Ulla", "surname": "Ullskog"},
     "2502039999": {"firstname": "Mohammed", "surname": "Larsson"},
-    "0502039999":{"firstname": "Bo", "surname": "Asp"}
+    "0502039999": {"firstname": "Bo", "surname": "Asp"},
 }
-ppl[] =
 
-# p(ppl.keys())  # som en lista med personnummer i detta fall
-
-p(ppl.values())
-# ger:
-# [
-#     {"firstname": "Torleif", "surname": "Bengtsson"},
-#     {"firstname": "Ulla", "surname": "Ullskog"},
-#     {"firstname": "Mohammed", "surname": "Larsson"},
-#     {"firstname": "Bo", "surname": "Asp"},
-# ]
+for pnr in ppl.keys():
+    p(pnr)
