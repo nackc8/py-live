89ba210a47bd5e1148d3c92f8cc63f27d6a6d1ef
Kent Larsson <kent@c8.se>
2025-03-03 10:26:33 +0100

    autocommit complete 1026 (33)

--- ITINF24/lektion7/list_queue.py
+++ ITINF24/lektion7/list_queue.py
@@ -1,8 +1,13 @@
 # listor som en kö
 
+# I 
+
 customer_queue = []
 
 customer_queue.append("Mayank")
 customer_queue.append("Prathiba")
 
-served = 
+served = customer_queue.pop(0)
+print(f"{served} Det blir ... kr")
+served = customer_queue.pop(0)
+print(f"{served} Det blir ... kr")
