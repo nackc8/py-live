b5c945c2e06170e3f9820ac389a6a7a1c6ae0747
Kent Larsson <kent@c8.se>
2025-03-03 10:59:34 +0100

    autocommit complete 1059 (34)

--- ITINF24/lektion7/del.py
+++ ITINF24/lektion7/del.py
@@ -1,4 +1,4 @@
-lst1 = ["björn", "anka", "groda", "myra"]
+lst1 = ["björn", "anka", "groda", "myra", "hund", "katt", "krokodil"]
 
 # fel, det blir samma målobjekt
 # lst2 = lst1
@@ -11,4 +11,4 @@ lst3 = lst1[:]
 # samma som: lst1.pop(1)
 
 # tilldela en ny del i lst3
-lst3[2:4] = ["hej"]
+lst3[2:4] = ["hej!!!", "Hallå!!!"]
