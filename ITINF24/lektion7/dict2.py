862c38c0b10f8406c4b98fce99715ecefe7d74bd
Kent Larsson <kent@c8.se>
2025-03-03 13:35:05 +0100

    autocommit complete 1335 (05)

--- ITINF24/lektion7/dict2.py
+++ ITINF24/lektion7/dict2.py
@@ -3,7 +3,7 @@ p = print
 # en dict med en nyckel, "4502129999" som har värdet None
 ppl_v0 = {"4502129999": None}
 
-ppl_v0 = {"4502129999": {"firstname": "Torleif"}}
+ppl_v0 = {"4502129999": {"firstname": "Torleif", "surname": "Bengtsson"}}
 
 
 p(ppl_v0)
