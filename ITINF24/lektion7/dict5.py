c752564f7e50068028850c5b88b9b082a1974d33
Kent Larsson <kent@c8.se>
2025-03-03 13:57:29 +0100

    autocommit complete 1357 (29)

--- ITINF24/lektion7/dict5.py
+++ ITINF24/lektion7/dict5.py
@@ -1,9 +1,9 @@
 p = print
 
 ppl = {
-    "4502129999": {"firstname": "Torleif", "surname": "Bengtsson"},
-    "1501019999": {"firstname": "Ulla", "surname": "Ullskog"},
-    "2502039999": {"firstname": "Mohammed", "surname": "Larsson"},
+    "194502129999": {"firstname": "Torleif", "surname": "Bengtsson"},
+    "191501019999": {"firstname": "Ulla", "surname": "Ullskog"},
+    "192502039999": {"firstname": "Mohammed", "surname": "Larsson"},
 }
 
 for pnr in ppl.keys():
