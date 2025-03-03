50e15471c1cefa4544ed1c3e786ab8f505778a51
Kent Larsson <kent@c8.se>
2025-03-03 13:49:50 +0100

    autocommit complete 1349 (50)

--- ITINF24/lektion7/dict4.py
+++ ITINF24/lektion7/dict4.py
@@ -2,13 +2,10 @@ import pprint
 
 p = pprint.pprint
 
-
 ppl = {
     "4502129999": {"firstname": "Torleif", "surname": "Bengtsson"},
     "1501019999": {"firstname": "Ulla", "surname": "Ullskog"},
     "2502039999": {"firstname": "Mohammed", "surname": "Larsson"},
 }
-
 ppl["0502039999"] = {"firstname": "Bo", "surname": "Asp"}
 
-p(ppl)
