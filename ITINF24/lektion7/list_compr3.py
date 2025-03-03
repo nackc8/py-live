a2caaff823d76cf3a4ed74cd0973feb68f3c3623
Kent Larsson <kent@c8.se>
2025-03-03 10:46:59 +0100

    autocommit complete 1046 (59)

--- ITINF24/lektion7/list_compr3.py
+++ ITINF24/lektion7/list_compr3.py
@@ -1,6 +1,3 @@
-ord = [""]
+ord = ["hej", "hejsan", "hoppsan", "myrstack"]
 
-# Krångligare?
-lst2 = [2**n + n for n in range(0, 10) if (2**n + n) % 2 == 0]
-
-# print("lika", lst1, lst2)
+ord_minst_7tkn = []
