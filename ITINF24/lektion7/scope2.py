37db90570453198be4965f307cad207187cf6278
Kent Larsson <kent@c8.se>
2025-03-03 14:35:37 +0100

    autocommit complete 1435 (37)

--- ITINF24/lektion7/scope2.py
+++ ITINF24/lektion7/scope2.py
@@ -9,3 +9,13 @@ direkt = scope2_called.x
 scope2_called.x = 333
 
 result2 = scope2_called.get_x()
+
+
+def hundra():
+    return 100
+
+
+# vi byter ut funktionen
+scope2_called.get_x = hundra
+
+result3 = scope2_called.get_x()
