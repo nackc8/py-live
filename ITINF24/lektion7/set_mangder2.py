4892dde13e779fb13f0bf51e7438dbaeb894bb49
Kent Larsson <kent@c8.se>
2025-03-03 12:54:05 +0100

    autocommit complete 1254 (05)

--- ITINF24/lektion7/set_mangder2.py
+++ ITINF24/lektion7/set_mangder2.py
@@ -1,6 +1,9 @@
 p = print
 
 words = {"burk", "bil", "kanot", "rep"}
+words2 = {"burk", "spis", "hamburgare"}
 
 p("bil in words?", "bil" in words)
 p("cykel in words?", "cykel" in words)
+
+p("words och words2 isdisjoint?", words.disjunct(words2))
