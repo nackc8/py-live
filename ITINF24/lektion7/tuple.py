aee2a2f81a109808334ffac329da33cbd095f22f
Kent Larsson <kent@c8.se>
2025-03-03 09:24:27 +0100

    autocommit complete 0924 (27)

--- ITINF24/lektion7/tuple.py
+++ ITINF24/lektion7/tuple.py
@@ -12,4 +12,9 @@ p("t1", type(t1), t1)
 
 p("name", type(name), name)
 
+# uttrycket till höger är en
+# tuple, men det försvinner direkt
+# då vi endast sparar delarna i
+# variablerna name, age.
 name, age = "Janne", 55
+
