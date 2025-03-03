0e9d59c1d8523edc32f6987ef71e5e85191f0273
Kent Larsson <kent@c8.se>
2025-03-03 10:45:06 +0100

    autocommit complete 1045 (06)

--- ITINF24/lektion7/list_compr2.py
+++ ITINF24/lektion7/list_compr2.py
@@ -4,6 +4,7 @@ for n in range(0, 10):
     if val % 2 == 0:
         lst1.append(val)
 
-lst2 = [2**n + n for n in range(0, 10) if 2**n + n % 2 == 0]
+# Krångligare?
+lst2 = [2**n + n for n in range(0, 10) if (2**n + n) % 2 == 0]
 
 # print("lika", lst1, lst2)
