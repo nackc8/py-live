52f4f14303449a8828af026a5e270bf7b7b987e6
Kent Larsson <kent@c8.se>
2025-03-03 10:34:07 +0100

    autocommit complete 1034 (07)

--- ITINF24/lektion7/list_compr.py
+++ ITINF24/lektion7/list_compr.py
@@ -1,6 +1,5 @@
+# lst1 = []
+# for n in range(0,10):
+#     lst1.append(2 ** n)
 
-lst1 = []
-for n in range(0,10):
-    lst1.append(2 ** n)
-
-lst2 = [n for n in range(0,10)]
+lst2 = [str(n) * 3 for n in range(0, 10)]
