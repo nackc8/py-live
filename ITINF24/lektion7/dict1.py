93f9e4cadebfdae4fe45dbe62397899004b8fcf6
Kent Larsson <kent@c8.se>
2025-03-03 13:29:18 +0100

    autocommit complete 1329 (18)

--- ITINF24/lektion7/dict1.py
+++ ITINF24/lektion7/dict1.py
@@ -8,6 +8,7 @@ p(type(x), x, len(x))
 # dict är lite som list här, indexet är ett ord istället för heltal i detta fall
 x["indoktrinera"] = "Styrd påverkan"
 x["pizza"] = "Platt mat"
+x[789] = "Ett högre än 788"
 
 p(f"indoktrinera betyder {x['indoktrinera']}")
 p(f"pizza betyder {x['pizza']}")
