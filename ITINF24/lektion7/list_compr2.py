4a29432c4830e5eb9017d363e89a58fffdb63cc3
Kent Larsson <kent@c8.se>
2025-03-03 10:41:03 +0100

    autocommit complete 1041 (03)

--- ITINF24/lektion7/list_compr2.py
+++ ITINF24/lektion7/list_compr2.py
@@ -1,7 +1,10 @@
 lst1 = []
-for n in range(0, 5):
-    lst1.append(2**n)
+for n in range(0, 10):
+    val = 2**n + n
+    if val % 2 == 0:
+        lst1.append(val)
 
-lst2 = [2**n for n in range(0, 5)]
 
-print("lika", lst1, lst2)
+# lst2 = [2**n for n in range(0, 5)]
+
+# print("lika", lst1, lst2)
