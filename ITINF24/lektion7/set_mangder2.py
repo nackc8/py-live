9e63913d2729d4ec4d15860d82fcaad6df87819e
Kent Larsson <kent@c8.se>
2025-03-03 12:49:56 +0100

    autocommit complete 1249 (56)

--- ITINF24/lektion7/set_mangder2.py
+++ ITINF24/lektion7/set_mangder2.py
@@ -1,16 +1,2 @@
 p = print
-# # Tillbakablick på hur datastrukturer skapas:
-# en_lst = ["x"]
-# en_tpl = ("x",)
-# en_rng = range(3, 8)
-# en_str = "x"
 
-# En mängd, ett set, skapas via
-# ett_tomt_set = set()
-# p(ett_tomt_set)
-# ett_set_med_innehall = set("hellohello")
-# p(ett_set_med_innehall)
-
-# tomt endast via: set()
-x = {"x", "x", "x", "x"}
-p(type(x), x) # <class 'set'> {'x'}
