c9ac6b24585c6a95ecec5db6ec00098a9243a6dc
Kent Larsson <kent@c8.se>
2025-03-03 12:55:52 +0100

    autocommit complete 1255 (52)

--- ITINF24/lektion7/set_mangder2.py
+++ ITINF24/lektion7/set_mangder2.py
@@ -2,8 +2,12 @@ p = print
 
 words = {"burk", "bil", "kanot", "rep"}
 words2 = {"burk", "spis", "hamburgare"}
+words3 = {"röd", "blå"}
 
 p("bil in words?", "bil" in words)
 p("cykel in words?", "cykel" in words)
 
-p("words och words2 isdisjoint?", words.disjunct(words2))
+# Mängdoperation: "Disjunkt" på sv, om två mängder inte har några
+# gemensamma element
+p("words och words2 isdisjoint?", words.isdisjoint(words2))
+p("words och words3 isdisjoint?", words.isdisjoint(words3))
