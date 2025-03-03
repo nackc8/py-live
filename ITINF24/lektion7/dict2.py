5f8ec09512b502effdfa49380808838c9a8f6270
Kent Larsson <kent@c8.se>
2025-03-03 13:41:46 +0100

    autocommit complete 1341 (46)

--- ITINF24/lektion7/dict2.py
+++ ITINF24/lektion7/dict2.py
@@ -2,15 +2,10 @@ import pprint
 
 p = pprint.pprint
 
-# en dict med en nyckel, "4502129999" som har värdet None
-ppl_v0 = {"4502129999": None}
-
-ppl_v1 = {
-    "4502129999": {"firstname": "Torleif", "surname": "Bengtsson"},
+ppl = {
+    "0502039999": {"firstname": "Bo", "surname": "Asp"},
     "1501019999": {"firstname": "Ulla", "surname": "Ullskog"},
     "2502039999": {"firstname": "Mohammed", "surname": "Larsson"},
+    "4502129999": {"firstname": "Torleif", "surname": "Bengtsson"},
 }
-
-ppl_v1["0502039999"] = {"firstname": "Bo", "surname": "Asp"}
-
-p(ppl_v1)
+p(ppl)
