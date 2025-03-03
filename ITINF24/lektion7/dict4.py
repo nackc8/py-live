fc4310402ca609501bc93fc8dc3445153e643b25
Kent Larsson <kent@c8.se>
2025-03-03 13:44:28 +0100

    autocommit complete 1344 (28)

--- ITINF24/lektion7/dict4.py
+++ ITINF24/lektion7/dict4.py
@@ -4,8 +4,10 @@ p = pprint.pprint
 
 # ppl har dict i sina values, deras values som är dict, har string
 
+bo = {"firstname": "Bo", "surname": "Asp"}
+
 ppl = {
-    "0502039999": {"firstname": "Bo", "surname": "Asp"},
+    "0502039999": bo,
     "1501019999": {"firstname": "Ulla", "surname": "Ullskog"},
     "2502039999": {"firstname": "Mohammed", "surname": "Larsson"},
     "4502129999": {"firstname": "Torleif", "surname": "Bengtsson"},
