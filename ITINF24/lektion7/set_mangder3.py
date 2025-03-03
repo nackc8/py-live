e10223bf796578dc5ab227aa31840785a83de2c2
Kent Larsson <kent@c8.se>
2025-03-03 13:02:04 +0100

    autocommit complete 1302 (04)

--- ITINF24/lektion7/set_mangder3.py
+++ ITINF24/lektion7/set_mangder3.py
@@ -4,11 +4,12 @@ words = {"burk", "bil", "kanot", "rep"}
 words2 = {"burk"}
 words3 = {"röd", "blå"}
 
-p("words2 issubset i words?", words2.issubset(words))
-p("words issubset i words2?", words.issubset(words2))
+# p("words2 issubset i words?", words2.issubset(words))
+# p("words issubset i words2?", words.issubset(words2))
 
-lst = ["a", "b", "c"]
+# lst = ["a", "b", "c"]
 
-# auto typecast till set inuti issubset()
-p(words2.issubset(swords))
+# # auto typecast till set inuti issubset()
+# p(words2.issubset(swords))
 
+p("difference words och word2", words.difference(words2))
