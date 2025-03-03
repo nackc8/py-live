2a52d4de5f87f6a684c0b0b5f322228b9bb8a9bc
Kent Larsson <kent@c8.se>
2025-03-03 14:14:15 +0100

    autocommit complete 1414 (15)

--- ITINF24/lektion7/dict7.py
+++ ITINF24/lektion7/dict7.py
@@ -6,7 +6,13 @@ ppl = {
     "192502039999": {"firstname": "Mohammed", "surname": "Larsson"},
 }
 
+# alt 1
+# for item in ppl.items():
+#     pnr = item[0]
+#     if "1915" in pnr:
+#         p(pnr, "heter", item[1]["firstname"])
+
 for item in ppl.items():
     pnr = item[0]
     if "1915" in pnr:
-        p(pnr)
+        p(pnr, "heter", item[1]["firstname"])
