c6522b961ec1bd964c71de8023ba402022c273d9
Kent Larsson <kent@c8.se>
2025-03-03 13:09:33 +0100

    autocommit complete 1309 (33)

--- ITINF24/lektion7/set_mangder4.py
+++ ITINF24/lektion7/set_mangder4.py
@@ -2,7 +2,15 @@ p = print
 
 lst = ["burk", "bil", "kanot", "bil", "rep"]
 
-# om ordningen inte spelar roll
-lst = list(set(lst))
+# # om ordningen inte spelar roll
+# lst_v1 = list(set(lst))
 
-p(lst)
+# p(lst_v1)
+
+# om ordningen spelar roll
+lst2 = []
+sett = set()
+for el in lst:
+    if el in sett:
+        
+
