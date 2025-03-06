ca9b8aa6d5a7f87e329e4839a84adba67d7dd6c2
Kent Larsson <kent@c8.se>
2025-03-06 09:40:33 +0100

    autocommit complete 0940 (33)

--- ITINF24/lektion9/refs_scope4.py
+++ ITINF24/lektion9/refs_scope4.py
@@ -5,20 +5,23 @@ lst2 = []
 
 listor = (lst1, lst2)
 
-minvar = 123  # Tilldela vanlig variabel
+
+def lst_add_print_factory():
+    # analogt till vanligt skapande: inner_lst_add_print = func
+    def inner_lst_add_print(ndx, obj):
+        lst = listor[ndx]
+        lst.append(obj)
+        print(lst)
+
+    return inner_lst_add_print
 
 
-def lst_add_print(ndx, obj):
-    lst = listor[ndx]
-    lst.append(obj)
-    print(lst)
+funk_igen = inner_lst_add_print
 
-funk_igen = lst_add_print
+inner_lst_add_print(1, "Hej")
 
-lst_add_print(1, "Hej")
-
-lst_add_print(0, "Hello2")
-lst_add_print(0, "Super")
-lst_add_print(0, "Mario")
-lst_add_print(0, "World")
-lst_add_print(1, "Benny!")
+inner_lst_add_print(0, "Hello2")
+inner_lst_add_print(0, "Super")
+inner_lst_add_print(0, "Mario")
+inner_lst_add_print(0, "World")
+inner_lst_add_print(1, "Benny!")
