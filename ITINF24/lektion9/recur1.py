c15548eba34741c885471568024dc8d73489d31e
Kent Larsson <kent@c8.se>
2025-03-06 15:11:58 +0100

    autocommit complete 1511 (58)

--- ITINF24/lektion9/recur1.py
+++ ITINF24/lektion9/recur1.py
@@ -11,6 +11,15 @@ global_lst = {
 }
 # fmt: on
 
+# Mål
+#
+# 1. Bakgrund
+# 1.1 Historia
+# 1.2 Nuläget
+# 2. Rapport
+# 2.1 Sverige
+# 2.2 Finland
+
 
 def indentera(flst):
     for rubrik, rubriksinnehall in flst.items():
