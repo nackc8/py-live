4755854008b9ea97b12a654091cefb960e4b3328
Kent Larsson <kent@c8.se>
2025-03-06 14:24:00 +0100

    autocommit complete 1424 (00)

---
+++ ITINF24/lektion9/except3.py
@@ -0,0 +1,10 @@
+# Inbyggda exceptions: https://docs.python.org/3.11/library/exceptions.html#concrete-exceptions
+
+
+class Hund:
+    def __init__(self, namn):
+        mening = f"Min hund {namn} är gooo!"
+
+
+# fel 1: Fel typ
+hund1 = Hund(34)
