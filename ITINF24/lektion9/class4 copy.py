d17448425207ef7b670c92f803d30566e26e56f9
Kent Larsson <kent@c8.se>
2025-03-06 12:55:25 +0100

    autocommit partial 1255 (25)

---
+++ ITINF24/lektion9/class4 copy.py
@@ -0,0 +1,41 @@
+p = print
+
+
+class Eagle:
+    total_sounds = 0
+
+    def __init__(self):
+        self.sounds = 0
+
+    def sound(self):
+        Eagle.total_sounds += 1
+        self.sounds += 1
+        stat = f"(tot: {Eagle.total_sounds}, ind: {self.sounds})"
+        print("Scriiish", stat)
+
+
+class Donkey:
+    total_sounds = 0
+
+    def __init__(self):
+        self.sounds = 0
+
+    def sound(self):
+        Donkey.total_sounds += 1
+        self.sounds += 1
+        stat = f"(tot: {Donkey.total_sounds}, ind: {self.sounds})"
+        print("Ihhh ohhh", stat)
+
+
+eagle1 = Eagle()
+eagle1.sound()
+
+eagle2 = Eagle()
+eagle2.sound()
+eagle2.sound()
+
+donkey = Donkey()
+donkey.sound()
+donkey.sound()
+donkey.sound()
+donkey.sound()
