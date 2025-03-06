6a308df621387f06e50c09bfdf11191059cc56a3
Kent Larsson <kent@c8.se>
2025-03-06 09:29:45 +0100

    autocommit complete 0929 (45)

--- ITINF24/lektion9/refs_scope2.py
+++ ITINF24/lektion9/refs_scope2.py
@@ -1,12 +1,21 @@
-lst = []
+lst1 = []
+lst2 = []
 
 
-def lst_add_print(obj):
-    lst.append(obj)
-    print(lst)
+def lst1_add_print(obj):
+    lst1.append(obj)
+    print(lst1)
 
 
-lst_add_print("Hello")
-lst_add_print("Super")
-lst_add_print("Mario")
-lst_add_print("World")
+def lst2_add_print(obj):
+    lst2.append(obj)
+    print(lst2)
+
+
+lst2_add_print("Hej")
+
+lst1_add_print("Hello")
+lst1_add_print("Super")
+lst1_add_print("Mario")
+lst1_add_print("World")
+lst2_add_print("Benny!")
