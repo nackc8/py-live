f08334ee23318d55c3141e82f1a99f60ade6ab31
Kent Larsson <kent@c8.se>
2025-03-06 14:39:34 +0100

    autocommit complete 1439 (34)

---
+++ ITINF24/lektion9/except5.py
@@ -0,0 +1,33 @@
+# Inbyggda exceptions: https://docs.python.org/3.11/library/exceptions.html#concrete-exceptions
+
+# Feltyper:
+
+# TypeError - fel datatyp
+# ValueError - rätt typ, men fel värde
+# Alla errors/exceptions ärver av Exception
+
+class InvalidDogNameError
+
+
+class Dog:
+    def __init__(self, name):
+        if not isinstance(name, str):
+            raise TypeError("Name must be a string")
+        if name == "":
+            raise ValueError("Empty name not allowed")
+
+        self.name = name
+        # Ger TypeError: can only concatenate str (not "int") to str
+
+    def get_sentence(self):
+        # TypeError: can only concatenate str (not "int") to str
+        sentence = "Min hund " + self.name + " är gooo!"
+        return sentence
+
+
+p = print
+
+# med raise smäller direkt
+hund1 = Dog("")
+
+p(hund1.get_sentence())
