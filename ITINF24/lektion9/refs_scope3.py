0beb6594ab0a23f1e1aefdff68f26377ea380b95
Kent Larsson <kent@c8.se>
2025-03-06 09:35:06 +0100

    autocommit complete 0935 (06)

--- ITINF24/lektion9/refs_scope3.py
+++ ITINF24/lektion9/refs_scope3.py
@@ -1,4 +1,4 @@
-# Skalar inte, eller hur?
+# Inte jättebra?
 
 lst1 = []
 lst2 = []
@@ -6,17 +6,16 @@ lst2 = []
 listor = (lst1, lst2)
 
 
-def lst_add_print(ndx,obj):
+def lst_add_print(ndx, obj):
     lst = listor[ndx]
     lst.append(obj)
     print(lst)
 
 
+lst_add_print(1, "Hej")
 
-lst_add_print(1,"Hej")
-
-lst_add_print(0,"Hello2")
-lst_add_print(0,"Super")
-lst_add_print(0,"Mario")
-lst_add_print(0,"World")
-lst_add_print(1,"Benny!")
+lst_add_print(0, "Hello2")
+lst_add_print(0, "Super")
+lst_add_print(0, "Mario")
+lst_add_print(0, "World")
+lst_add_print(1, "Benny!")
