a6f52ad269a0dbca2ba58ee781567e164337d6a2
Kent Larsson <kent@c8.se>
2025-03-06 14:20:16 +0100

    autocommit complete 1420 (16)

--- ITINF24/lektion9/except2.py
+++ ITINF24/lektion9/except2.py
@@ -1,12 +1,17 @@
-standard_input = "hej"
+standard_input = "h"
 
 # Inbyggda exceptions: https://docs.python.org/3.11/library/exceptions.html#concrete-exceptions
+
+
 
 try:
     x = int(input("Skriv ett tal:"))
     resultat = 10 / x
-    print(resultat)
 except ZeroDivisionError as e:
     print("fel:", e)
+    resultat = 1
 except Exception as e:
     print("okänt fel:", e)
+    resultat = 0  # fix? Inga fel nedan ivf
+
+print(resultat)
