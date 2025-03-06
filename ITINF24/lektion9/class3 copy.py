6609a20ea134bc6bbe23c8cad68cff967f83a0eb
Kent Larsson <kent@c8.se>
2025-03-06 12:41:56 +0100

    autocommit partial 1241 (56)

---
+++ ITINF24/lektion9/class3 copy.py
@@ -0,0 +1,18 @@
+p = print
+
+
+class Eagle:
+    def sound(self):
+        print("Scriiish")
+
+
+class Donkey:
+    def sound(self):
+        print("Ihhh ohhh")
+
+
+eagle1 = Eagle()
+eagle1.sound()
+
+donkey = Donkey()
+donkey.sound()
