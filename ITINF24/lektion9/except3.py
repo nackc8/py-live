caa7967faf500eccd920ee31f952d18697e0425e
Kent Larsson <kent@c8.se>
2025-03-06 14:32:08 +0100

    autocommit complete 1432 (08)

--- ITINF24/lektion9/except3.py
+++ ITINF24/lektion9/except3.py
@@ -8,7 +8,7 @@
 class Hund:
     def __init__(self, name):
         if not isinstance(name, str):
-            print("NEJ! INGEN STRING")
+            raise 
 
         self.name = name
         # Ger TypeError: can only concatenate str (not "int") to str
