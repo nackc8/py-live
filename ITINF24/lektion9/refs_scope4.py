d866ddaf890177d6dd768e57f98ba2b003045c8c
Kent Larsson <kent@c8.se>
2025-03-06 09:42:10 +0100

    autocommit complete 0942 (10)

--- ITINF24/lektion9/refs_scope4.py
+++ ITINF24/lektion9/refs_scope4.py
@@ -7,20 +7,22 @@ listor = (lst1, lst2)
 
 
 def lst_add_print_factory():
-    # analogt till vanligt skapande: inner_lst_add_print = func
-    def inner_lst_add_print(ndx, obj):
+    # analogt till vanligt skapande: inner = func
+    def inner(ndx, obj):
         lst = listor[ndx]
         lst.append(obj)
         print(lst)
 
-    return inner_lst_add_print
+    return inner
 
+
+inner_lst_add_print = lst_add_print_factory()
 
 funk_igen = inner_lst_add_print
 
 inner_lst_add_print(1, "Hej")
 
-inner_lst_add_print(0, "Hello2")
+inner_lst_add_print(0, "Hello")
 inner_lst_add_print(0, "Super")
 inner_lst_add_print(0, "Mario")
 inner_lst_add_print(0, "World")
