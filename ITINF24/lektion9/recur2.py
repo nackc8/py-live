eb7f3cdec075f178197123863deab1ef89ab0115
Kent Larsson <kent@c8.se>
2025-03-06 15:38:51 +0100

    autocommit complete 1538 (51)

---
+++ ITINF24/lektion9/recur2.py
@@ -0,0 +1,33 @@
+# fmt: off
+global_lst = {
+    "Bakgrund": {
+        "Historia": False,
+        "Nuläget": False
+    },
+    "Rapport": {
+        "Sverige": False,
+        "Finland": False
+    }
+}
+# fmt: on
+
+# Mål
+#
+# 1. Bakgrund
+# 1.1 Historia
+# 1.2 Nuläget
+# 2. Rapport
+# 2.1 Sverige
+# 2.2 Finland
+
+
+def indentera(lst, prefix=""):
+    n = 0
+    for header, content in lst.items():
+        n += 1
+        print(prefix, n, " ", header, sep="")
+        if content:
+            indentera(content, f"{str(n)}.")
+
+
+indentera(global_lst)
