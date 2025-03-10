7294ed8295afeb40172733462570d0d4bafa480c
Kent Larsson <kent@c8.se>
2025-03-10 10:37:27 +0100

    autocommit complete 1037 (27)

--- ITINF24/lektion10/open3.py
+++ ITINF24/lektion10/open3.py
@@ -2,7 +2,7 @@ import sys
 
 # Vi läser in en fil med en rad som är bredare än 100
 myfile = open(
-    "/shared/kent/Documents/nackademin/python/py-live/ITINF24/lektion10/langfil.txt"
+    "/shared/kent/Documents/nackademin/python/py-live/ITINF24/lektion10/produkter.txt"
 )
 
 # endast de 100 första tecknen
