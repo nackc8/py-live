b1ff3d9527e79d5ac559255e3c1983ad8dd74e53
Kent Larsson <kent@c8.se>
2025-03-10 11:10:05 +0100

    autocommit complete 1110 (05)

--- ITINF24/lektion10/read_write1.py
+++ ITINF24/lektion10/read_write1.py
@@ -7,7 +7,7 @@ file_path = Path(__file__).with_name("nummer.txt")
 # Vi ska skriva.
 myfile = open(file_path, mode="w")
 
-for _ range(100):
+for _ in range(100):
     myfile.write("More! ")
 
 input("Hello")
