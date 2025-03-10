edbc151649cd3e5277671416cbb37c13494b4568
Kent Larsson <kent@c8.se>
2025-03-10 10:49:20 +0100

    autocommit complete 1049 (20)

--- ITINF24/lektion10/open4.py
+++ ITINF24/lektion10/open4.py
@@ -3,7 +3,7 @@ import sys
 finfil = open(
     "/shared/kent/Documents/nackademin/python/py-live/ITINF24/lektion10/produkter.txt"
 )
-
+#
 hela_filen_i_en_var = finfil.read()
 
 print(hela_filen_i_en_var)
