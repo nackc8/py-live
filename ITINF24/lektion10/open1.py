700a44310e4a79ad279f10c507049dc5c09c06c6
Kent Larsson <kent@c8.se>
2025-03-10 10:19:35 +0100

    autocommit complete 1019 (35)

--- ITINF24/lektion10/open1.py
+++ ITINF24/lektion10/open1.py
@@ -5,6 +5,10 @@ myfile = open(
     "/shared/kent/Documents/nackademin/python/py-live/ITINF24/lektion10/produkter.txt"
 )
 
+# Läs fyra första tecknen
 fyra_forsta = myfile.read(4)
+
+rader = myfile.
+
 
 input("Press enter...")
