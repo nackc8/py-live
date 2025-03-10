64c84b4ab11d11494b1a34a31dd6e864df83bb16
Kent Larsson <kent@c8.se>
2025-03-10 10:38:19 +0100

    autocommit complete 1038 (19)

--- ITINF24/lektion10/open3.py
+++ ITINF24/lektion10/open3.py
@@ -1,18 +1,10 @@
 import sys
 
-# Vi läser in en fil med en rad som är bredare än 100
-myfile = open(
+finfil = open(
     "/shared/kent/Documents/nackademin/python/py-live/ITINF24/lektion10/produkter.txt"
 )
 
-# endast de 100 första tecknen
-rad1 = myfile.readline(100)
-
-# läser vidare på samma rad tills radslut
-rad2 = myfile.readline(100)
-
-# nu läser den rad 2
-rad3 = myfile.readline(100)
+finfil.
 
 
 input("Press enter...")
