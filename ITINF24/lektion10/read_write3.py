d37543c7b472af8cabd06b25445a48ebd809cb01
Kent Larsson <kent@c8.se>
2025-03-10 11:17:40 +0100

    autocommit complete 1117 (40)

---
+++ ITINF24/lektion10/read_write3.py
@@ -0,0 +1,16 @@
+# Bara för att hitta filen i samma katalog som vår källkod, vi
+# tittar på detta senare..
+from pathlib import Path
+
+file_path = Path(__file__).with_name("nummer.txt")
+
+# Vi ska skriva.
+myfile = open(file_path, mode="w")
+
+for _ in range(100):
+    
+    myfile.write("More! ")
+
+myfile.close()
+
+input("Hello")
