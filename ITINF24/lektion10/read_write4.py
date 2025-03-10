f96f4766c5dba42d7ce1cb3ff5ab83025d6d3cf0
Kent Larsson <kent@c8.se>
2025-03-10 11:28:39 +0100

    autocommit complete 1128 (39)

--- ITINF24/lektion10/read_write4.py
+++ ITINF24/lektion10/read_write4.py
@@ -8,6 +8,8 @@ file_path = Path(__file__).with_name("nummer.txt")
 
 with open(file_path, mode="w") as myfile:
     # Använd gärna en conditional breakpoint: i == 97
+    # för att slippa klicka step over manuellt 97 gånger,
+    # om vi är ute efter att se de sista snurren i loopen.
     for i in range(100):
         myfile.write("More! ")
 
