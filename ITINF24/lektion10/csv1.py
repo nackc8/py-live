638440c95564777a085f7ab3edb084560ada13bc
Kent Larsson <kent@c8.se>
2025-03-10 13:11:18 +0100

    autocommit complete 1311 (18)

--- ITINF24/lektion10/csv1.py
+++ ITINF24/lektion10/csv1.py
@@ -1,4 +1,11 @@
 import csv
 from pathlib import Path
 
-file_path = Path(__file__).with_name("produkter.txt")
+file_path = Path(__file__).with_name("inlamningsuppgifter.csv")
+
+p = print
+
+# Testa att den finns
+# p("Finns? ", file_path.exists())
+
+with open()
