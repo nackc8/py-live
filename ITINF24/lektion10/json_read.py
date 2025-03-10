06a54554c49b1b2ee0303ddf0061c5dbd775a625
Kent Larsson <kent@c8.se>
2025-03-10 13:35:46 +0100

    autocommit complete 1335 (46)

--- ITINF24/lektion10/json_read.py
+++ ITINF24/lektion10/json_read.py
@@ -2,4 +2,8 @@ import json
 
 json_content = '{ "greeting": "hello"  }'
 
-json_loaded = json.load(json_content)
+json_loaded = json.loads(json_content)
+
+print(type(json_loaded))
+
+print(json_loaded)
