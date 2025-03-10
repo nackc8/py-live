7646b1bfbaba86b68b321aabe856c895b5494583
Kent Larsson <kent@c8.se>
2025-03-10 12:50:51 +0100

    autocommit complete 1250 (51)

--- ITINF24/lektion10/pathlib_path.py
+++ ITINF24/lektion10/pathlib_path.py
@@ -7,6 +7,6 @@ p("cwd", cwd)
 cwd_parent_dir = cwd.parent
 p("cwd_parent_dir", cwd_parent_dir)
 
-p("dirname", cwd.)
+p("dirname", cwd.name)
 # p("basename", os.path.basename(cwd))
 # p("exists(/etc/ssh/sshd_config)", os.path.exists("/etc/ssh/sshd_config"))
