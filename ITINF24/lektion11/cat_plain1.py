a6586e887468d67c45902ce0d9521c4f88976589
Kent Larsson <kent@c8.se>
2025-03-12 09:52:04 +0100

    autocommit complete 0952 (04)

--- ITINF24/lektion11/cat_plain1.py
+++ ITINF24/lektion11/cat_plain1.py
@@ -2,6 +2,13 @@
 import sys
 
 #   cat [OPTION]... [FILE]...
+# -b, --number-nonblank
+# -n, --number
+# -s, --squeeze-blank
+  --help display this help and exit
+
+       --version
+              output version information and exit
 
 
 print(sys.argv)
