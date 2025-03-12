8c1dca2d4f62acaa6e26305ef0c5b8ef8ac3dbcf
Kent Larsson <kent@c8.se>
2025-03-12 13:09:42 +0100

    autocommit partial 1309 (42)

---
+++ ITINF24/lektion11/lion_alfa2 copy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(
+    prog="Lion",
+    description="Like Cat, but slower and with more functionality",
+    epilog="End of description",
+)
+
+parser.add_argument("filename", help="The filename to show")
+values = parser.parse_args(sys.argv[1:])
+
+print("filename:", values.filename)
