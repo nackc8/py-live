ae279c5656cf92121958e1f0d71a349ae2a90d58
Kent Larsson <kent@c8.se>
2025-03-12 10:13:27 +0100

    autocommit complete 1013 (27)

--- ITINF24/lektion11/cat_plain2.py
+++ ITINF24/lektion11/cat_plain2.py
@@ -5,17 +5,21 @@ if len(sys.argv) < 2:
     print("Får inte vara tomt!", file=sys.stderr)
     sys.exit(1)
 
-filename = sys.argv[1]
-
 # for arg in sys.argv[1:]:
 #     print("arg", arg)
 
 show_filename = False
 if sys.argv[1] == "-f" or sys.argv[1] == "--show-filename":
+    filename = sys.argv[2]
     show_filename = True
+else:
+    filename = sys.argv[1]
+
 
 with open(filename) as fp:
     content = fp.readlines()
 
-# for line in content:
-#     print(line, end="")
+
+
+for line in content:
+    print(line, end="")
