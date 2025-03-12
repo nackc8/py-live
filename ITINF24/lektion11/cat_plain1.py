53512158b4a3a5b1a5163328789a380a80e87bd7
Kent Larsson <kent@c8.se>
2025-03-12 10:02:53 +0100

    autocommit complete 1002 (53)

--- ITINF24/lektion11/cat_plain1.py
+++ ITINF24/lektion11/cat_plain1.py
@@ -5,4 +5,9 @@ if len(sys.argv) != 2:
     print("Fel antal arg!", file=sys.stderr)
     sys.exit(1)
 
-print(sys.argv)
+filename = sys.argv[1]
+
+with open(filename) as fp:
+    content = fp.
+
+print("filnamn", filename)
