641ccb0cbdaa9085d4310bc4b1e5fe72297cfb67
Kent Larsson <kent@c8.se>
2025-03-12 13:03:30 +0100

    autocommit complete 1303 (30)

--- ITINF24/lektion11/lion_alfa1.py
+++ ITINF24/lektion11/lion_alfa1.py
@@ -8,22 +8,8 @@ parser = argparse.ArgumentParser(
     epilog="End of description",
 )
 
-parser.add_argument('filename')
+parser.add_argument("filename")
+
 
 parser.parse_args(sys.argv[1:])
 
-
-# for filename in filenames:
-#     with open(filename) as fp:
-#         contentlines = fp.readlines()
-
-#     if show_filename:
-#         print(filename, ":", sep="", end="\n\n")
-
-#     for lineno_line_tuple in enumerate(contentlines, 1):
-#         lineno = lineno_line_tuple[0]
-#         line = lineno_line_tuple[1]
-#         if show_linenumbers:
-#             print(lineno, line, end="")
-#         else:
-#             print(line, end="")
