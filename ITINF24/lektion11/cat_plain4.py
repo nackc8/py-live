c957182abdc6d8016bb12acd3c034c6468f3b451
Kent Larsson <kent@c8.se>
2025-03-12 11:09:36 +0100

    autocommit complete 1109 (36)

---
+++ ITINF24/lektion11/cat_plain4.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import sys
+
+if len(sys.argv) < 2:
+    print("Får inte vara tomt!", file=sys.stderr)
+    sys.exit(1)
+
+show_filename = False
+
+# -n, --number
+
+args = sys.argv[1:]
+used_args_count = 0
+for arg in args:
+    if arg == "-f" or arg == "--show-filename":
+        show_filename = True
+        used_args_count += 1
+    elif arg == "--":
+        used_args_count += 1
+        break
+    else:
+        break
+
+# Ta bort flaggorna från loopen ovan, och behåll endast filnamnen.
+filenames = args[used_args_count:]
+
+for filename in filenames:
+    with open(filename) as fp:
+        content = fp.readlines()
+
+    # lite nytt: är samma som att skriva
+    # if show_filename == True
+    if show_filename:
+        print(filename, ":", sep="", end="\n\n")
+
+    for line in content:
+        print(line, end="")
