b8a33c2d85ae329abcd7506552e2802c2971de99
Kent Larsson <kent@c8.se>
2025-03-12 13:38:54 +0100

    autocommit complete 1338 (54)

--- ITINF24/lektion11/lionlogic_alfa2.py
+++ ITINF24/lektion11/lionlogic_alfa2.py
@@ -2,22 +2,22 @@ def process(filename):
     return [filename, "hej", "jopp"]
 
 
-for filename in filenames:
-    with open(filename) as fp:
-        contentlines = fp.readlines()
+# for filename in filenames:
+#     with open(filename) as fp:
+#         contentlines = fp.readlines()
 
-    # lite nytt: är samma som att skriva
-    # if show_filename == True
-    if show_filename:
-        print(filename, ":", sep="", end="\n\n")
+#     # lite nytt: är samma som att skriva
+#     # if show_filename == True
+#     if show_filename:
+#         print(filename, ":", sep="", end="\n\n")
 
-    # alt sätt: (med unboxing av tupeln)
-    # for lineno, line in enumerate(contentlines, 1):
+#     # alt sätt: (med unboxing av tupeln)
+#     # for lineno, line in enumerate(contentlines, 1):
 
-    for lineno_line_tuple in enumerate(contentlines, 1):
-        lineno = lineno_line_tuple[0]
-        line = lineno_line_tuple[1]
-        if show_linenumbers:
-            print(lineno, line, end="")
-        else:
-            print(line, end="")
+#     for lineno_line_tuple in enumerate(contentlines, 1):
+#         lineno = lineno_line_tuple[0]
+#         line = lineno_line_tuple[1]
+#         if show_linenumbers:
+#             print(lineno, line, end="")
+#         else:
+#             print(line, end="")
