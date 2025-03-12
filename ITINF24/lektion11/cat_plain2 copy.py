d55de575399c679170d3df1faf438dd8d0484f9e
Kent Larsson <kent@c8.se>
2025-03-12 10:19:17 +0100

    autocommit partial 1019 (17)

---
+++ ITINF24/lektion11/cat_plain2 copy.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import sys
+
+if len(sys.argv) < 2:
+    print("Får inte vara tomt!", file=sys.stderr)
+    sys.exit(1)
+
+# for arg in sys.argv[1:]:
+#     print("arg", arg)
+
+show_filename = False
+if sys.argv[1] == "-f" or sys.argv[1] == "--show-filename":
+    filename = sys.argv[2]
+    show_filename = True
+else:
+    filename = sys.argv[1]
+
+
+with open(filename) as fp:
+    content = fp.readlines()
+
+# lite nytt: är samma som att skriva
+# if show_filename == True
+if show_filename:
+    print(filename, ":", sep="", end="\n\n")
+
+for line in content:
+    print(line, end="")
