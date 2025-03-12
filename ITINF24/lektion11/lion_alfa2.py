3810327a0d7d31c8d5912f2b323601499a6f1f8e
Kent Larsson <kent@c8.se>
2025-03-12 13:20:49 +0100

    autocommit complete 1320 (49)

--- ITINF24/lektion11/lion_alfa2.py
+++ ITINF24/lektion11/lion_alfa2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import sys
+from pathlib import Path
 
 import lionlogic_alfa2 as logic
 
@@ -13,7 +14,12 @@ parser = argparse.ArgumentParser(
 parser.add_argument("filename", help="The filename to show")
 values = parser.parse_args(sys.argv[1:])
 
-output_list = logic.process(values.filename)
+filepath = Path(values.filename)
+
+if not filepath.exists():
+    print("Error: File does not exist: ")
+
+output_list = logic.process(filepath)
 
 for line in output_list:
     print(line)
