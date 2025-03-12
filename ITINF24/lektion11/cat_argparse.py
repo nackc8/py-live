8be7f24254e6073fa930a0d0cee8fe28da0cf651
Kent Larsson <kent@c8.se>
2025-03-12 11:24:28 +0100

    autocommit complete 1124 (28)

--- ITINF24/lektion11/cat_argparse.py
+++ ITINF24/lektion11/cat_argparse.py
@@ -1,56 +1,19 @@
 #!/usr/bin/env python3
 import sys
 
-if len(sys.argv) < 2:
-    print("Får inte vara tomt!", file=sys.stderr)
-    sys.exit(1)
 
-show_filename = False
-show_linenumbers = False
 
-# -n, --number
+# for filename in filenames:
+#     with open(filename) as fp:
+#         contentlines = fp.readlines()
 
-args = sys.argv[1:]
-used_args_count = 0
-for arg in args:
-    if arg == "-f" or arg == "--show-filename":
-        show_filename = True
-        used_args_count += 1
-    elif arg == "-n" or arg == "--number":
-        show_linenumbers = True
-        used_args_count += 1
-    # -- separerar flaggor från filnamn
-    #  ./cat_plain4.py -n -- --show-filename  funkar nu, då --show-filename tolkas
-    # som ett filnamn.
-    elif arg == "--":
-        used_args_count += 1
-        break
-    else:
-        break
+#     if show_filename:
+#         print(filename, ":", sep="", end="\n\n")
 
-# Ta bort flaggorna från loopen ovan, och behåll endast filnamnen.
-filenames = args[used_args_count:]
-
-if len(filenames) == 0:
-    print("Hörru! Ange minst ett filnamn", file=sys.stderr)
-    sys.exit(1)
-
-for filename in filenames:
-    with open(filename) as fp:
-        contentlines = fp.readlines()
-
-    # lite nytt: är samma som att skriva
-    # if show_filename == True
-    if show_filename:
-        print(filename, ":", sep="", end="\n\n")
-
-    # alt sätt: (med unboxing av tupeln)
-    # for lineno, line in enumerate(contentlines, 1):
-
-    for lineno_line_tuple in enumerate(contentlines, 1):
-        lineno = lineno_line_tuple[0]
-        line = lineno_line_tuple[1]
-        if show_linenumbers:
-            print(lineno, line, end="")
-        else:
-            print(line, end="")
+#     for lineno_line_tuple in enumerate(contentlines, 1):
+#         lineno = lineno_line_tuple[0]
+#         line = lineno_line_tuple[1]
+#         if show_linenumbers:
+#             print(lineno, line, end="")
+#         else:
+#             print(line, end="")
