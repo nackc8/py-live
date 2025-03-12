c789a1cad822abbf5a187a9bdce6f8d8eb6e1d5e
Kent Larsson <kent@c8.se>
2025-03-12 09:54:12 +0100

    autocommit complete 0954 (12)

--- ITINF24/lektion11/cat_plain1.py
+++ ITINF24/lektion11/cat_plain1.py
@@ -5,7 +5,10 @@ import sys
 # -b, --number-nonblank
 # -n, --number
 # -s, --squeeze-blank
-# -x ORD
+# -f --show-filename
+# -i --indent content
+# --indent-symbol SYMBOL
+#
 #   --help display this help and exit
 #        --version
 #               output version information and exit
