3345d5f1f03b534d37162c87a68a15e6f447dded
Kent Larsson <kent@c8.se>
2025-03-13 14:13:59 +0100

    autocommit partial 1413 (59)

---
+++ ITINF24/lektion12/unittest copy/cat_math_test.py
@@ -0,0 +1,24 @@
+import unittest
+
+from cat_math import human_to_cat_years as htc
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_one(self):
+        self.assertEqual(htc(1), 15)
+
+    def test_two(self):
+        self.assertEqual(htc(2), 24)
+
+    def test_three(self):
+        self.assertEqual(htc(3), 28)
+
+    def test_four(self):
+        self.assertEqual(htc(4), 32)
+
+    def test_eighteen(self):
+        self.assertEqual(htc(18), 88)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
