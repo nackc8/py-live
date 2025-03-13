9291f6bc5ab60288d9025b67206dc0c2dcbcf478
Kent Larsson <kent@c8.se>
2025-03-13 12:08:47 +0100

    autocommit complete 1208 (47)

--- ITINF24/lektion12/cat_years.py
+++ ITINF24/lektion12/cat_years.py
@@ -1,1 +1,11 @@
-The first year of your cat's life is considered the equivalent of 15 human years. The second year adds nine more human years, so 2 cat years are approximately 24 human years. After this, each additional human year is four “cat years” i.e., age 3 will be 28 human years.7'
+The first year of your cat's
+life is considered the
+equivalent of 15 human years.
+The second year adds nine
+more human years, so 2 cat
+years are approximately 24
+human years. After this, each
+additional human year is four
+“cat years” i.e., age 3
+will be 28 human years.7''
+'
