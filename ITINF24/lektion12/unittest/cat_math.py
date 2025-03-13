506daf699ab08fae7c4573779966a2139ec44d7e
Kent Larsson <kent@c8.se>
2025-03-13 13:23:36 +0100

    autocommit complete 1323 (36)

--- ITINF24/lektion12/unittest/cat_math.py
+++ ITINF24/lektion12/unittest/cat_math.py
@@ -1,7 +1,7 @@
 def human_to_cat_years(human_years):
     if human_years == 1:
         return 15
-    elif human_years == 3:
-        return 28
-    else:
+    elif human_years == 2:
         return 24
+    else:
+        return 28
