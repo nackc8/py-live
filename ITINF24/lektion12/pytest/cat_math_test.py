4cde4a659e9b67d138ee30f1cd4b077a9e265b66
Kent Larsson <kent@c8.se>
2025-03-13 14:19:53 +0100

    autocommit complete 1419 (53)

--- ITINF24/lektion12/pytest/cat_math_test.py
+++ ITINF24/lektion12/pytest/cat_math_test.py
@@ -1,29 +1,24 @@
 from cat_math import human_to_cat_years as htc
 
 
-def prod(a, b):
-    assert isinstance(a, int)
-    return a * b
-
-
 def test_one():
-    self.assertEqual(htc(1), 15)
+    assert htc(1), 15
 
 
 def test_two():
-    self.assertEqual(htc(2), 24)
+    assert htc(2), 24
 
 
 def test_three():
-    self.assertEqual(htc(3), 28)
+    assert htc(3), 28
 
 
 def test_four():
-    self.assertEqual(htc(4), 32)
+    assert htc(4), 32
 
 
 def test_eighteen():
-    self.assertEqual(htc(18), 88)
+    assert htc(18), 88
 
 
 if __name__ == "__main__":
