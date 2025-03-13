06df1d777e01fc1d12d378c7cb7c065ab8eae85f
Kent Larsson <kent@c8.se>
2025-03-13 14:16:15 +0100

    autocommit complete 1416 (15)

--- ITINF24/lektion12/pytest/cat_math_test.py
+++ ITINF24/lektion12/pytest/cat_math_test.py
@@ -1,21 +1,24 @@
 from cat_math import human_to_cat_years as htc
 
 
-class TestStringMethods(unittest.TestCase):
-    def test_one(self):
-        self.assertEqual(htc(1), 15)
+def test_one():
+    self.assertEqual(htc(1), 15)
 
-    def test_two(self):
-        self.assertEqual(htc(2), 24)
 
-    def test_three(self):
-        self.assertEqual(htc(3), 28)
+def test_two():
+    self.assertEqual(htc(2), 24)
 
-    def test_four(self):
-        self.assertEqual(htc(4), 32)
 
-    def test_eighteen(self):
-        self.assertEqual(htc(18), 88)
+def test_three():
+    self.assertEqual(htc(3), 28)
+
+
+def test_four():
+    self.assertEqual(htc(4), 32)
+
+
+def test_eighteen():
+    self.assertEqual(htc(18), 88)
 
 
 if __name__ == "__main__":
