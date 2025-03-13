e746dbdb3b944fb5289caa98785b2e26efa4f252
Kent Larsson <kent@c8.se>
2025-03-13 11:08:49 +0100

    autocommit complete 1108 (49)

--- ITINF24/lektion12/doctests/greet.py
+++ ITINF24/lektion12/doctests/greet.py
@@ -5,8 +5,14 @@
 def greet(name=None):
     """Greets a fellow or named person
 
+    >>> greet("Urban")
+    Hello Urban!
     """
     if name is None:
         return "Hello stranger!"
     else:
         return f"Hello {name}!"
+
+if __name__ == "__main__":
+    import doctest
+    
