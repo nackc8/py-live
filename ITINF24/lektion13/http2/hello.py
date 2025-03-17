ce7017674d1cef2c03b584c12513c6753f1c4a56
Kent Larsson <kent@c8.se>
2025-03-17 10:23:39 +0100

    autocommit complete 1023 (39)

--- ITINF24/lektion13/http2/hello.py
+++ ITINF24/lektion13/http2/hello.py
@@ -1,16 +1,22 @@
+from flask import Flask
 from markupsafe import escape
 
-@app.route('/user/<username>')
+app = Flask(__name__)
+
+
+@app.route("/user/<username>")
 def show_user_profile(username):
     # show the user profile for that user
-    return f'User {escape(username)}'
+    return f"User {escape(username)}"
 
-@app.route('/post/<int:post_id>')
+
+@app.route("/post/<int:post_id>")
 def show_post(post_id):
     # show the post with the given id, the id is an integer
-    return f'Post {post_id}'
+    return f"Post {post_id}"
 
-@app.route('/path/<path:subpath>')
+
+@app.route("/path/<path:subpath>")
 def show_subpath(subpath):
     # show the subpath after /path/
-    return f'Subpath {escape(subpath)}'
+    return f"Subpath {escape(subpath)}"
