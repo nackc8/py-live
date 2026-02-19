a6b39ba5ac24530e963a4a4a281b96bdb019574d
Kent Larsson <kent@c8.se>
2026-02-19 14:07:51 +0100

    autocommit complete 1407 (51)

--- ITINF25/lektion1/loop.py
+++ ITINF25/lektion1/loop.py
@@ -1,7 +1,8 @@
 bil_lista = ["Mazda", "Volvo", "Opel"]
 
-print(bil_lista)
+# print(bil_lista)
 
 # Loopa! För att gå igenom varje sak... kallas formellt element
 
-for bil in bil_lista
+for bil in bil_lista:
+    print(bil)
