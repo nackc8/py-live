514ed2447dcff35c539ad9f457804df9ce1cc6e8
Kent Larsson <kent@c8.se>
2026-02-20 13:23:33 +0100

    autocommit complete 1323 (33)

--- ITINF25/lektion2/immutable_vs_mutable.py
+++ ITINF25/lektion2/immutable_vs_mutable.py
@@ -2,7 +2,9 @@
 # ändras när man skapat. Eller så kan den vara
 # mutable, då kan den ändras.
 
-obj1_id = id("hej")
-obj2_id = id("hej")
+obj1 = "hej"
+obj2 = "hej"
+obj1_id = id(obj1)
+obj2_id = id(obj2)
 
 print(obj1_id, obj2_id)
