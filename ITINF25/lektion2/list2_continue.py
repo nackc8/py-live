8e1a4dd686553f1711aff5743c9ad3ff8a535082
Kent Larsson <kent@c8.se>
2026-02-20 14:24:31 +0100

    autocommit complete 1424 (31)

--- ITINF25/lektion2/list2_continue.py
+++ ITINF25/lektion2/list2_continue.py
@@ -3,6 +3,8 @@ checkout_list = [30, 100, 20, 5, 5, 90]
 customer_wallet = 200
 total = 0
 for grocery in checkout_list:
+    if grocery < 50:
+        continue
     if customer_wallet < total + grocery:
         break  # AVBRYT NU!
     total = total + grocery
