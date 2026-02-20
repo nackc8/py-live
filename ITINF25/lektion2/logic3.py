e2f0553e3296a9ea592167836a7f5c24146c565d
Kent Larsson <kent@c8.se>
2026-02-20 10:14:23 +0100

    autocommit complete 1014 (23)

--- ITINF25/lektion2/logic3.py
+++ ITINF25/lektion2/logic3.py
@@ -2,9 +2,9 @@ tal1 = -100
 tal2 = 200
 tal3 = 0
 
-# EJ: if tal1 < tal2 and tal3:
-# KANSKE TYDLIGARE MED? if (tal1 < tal2) and (tal1 < tal3):
-if tal1 < tal2 and tal1 < tal3:
+x = tal1 < tal2
+y = tal1 < tal3
+if x and y:
     print("tal1 är mindre än både tal2 och tal3")
 else:
     print("tal1 är inte bra!")
