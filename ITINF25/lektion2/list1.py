bf0d009c0fa752a4912dc2d66303a00c303d16c9
Kent Larsson <kent@c8.se>
2026-02-20 14:20:26 +0100

    autocommit complete 1420 (26)

--- ITINF25/lektion2/list1.py
+++ ITINF25/lektion2/list1.py
@@ -3,5 +3,10 @@ checkout_list = [30, 100, 20, 5, 5, 90]
 customer_wallet = 200
 total = 0
 for grocery in checkout_list:
+    if customer_wallet < total + grocery:
+        break  # AVBRYT NU!
     total = total + grocery
     print("Sliding total:", total)
+
+print("---------")
+print("Final total", total)
