049d6a802ffe23da72844e71458565c0314ffc01
Kent Larsson <kent@c8.se>
2026-02-20 14:18:17 +0100

    autocommit complete 1418 (17)

--- ITINF25/lektion2/list1.py
+++ ITINF25/lektion2/list1.py
@@ -1,6 +1,6 @@
 checkout_list = [30, 100, 20, 5, 5, 90]
 
 total = 0
-for item in checkout_list:
-    total = total + item
+for grocery in checkout_list:
+    total = total + grocery
     print("Sliding total:", total)
