fa456759be1c1199dc96d14e18838d5c437d7a3d
Kent Larsson <kent@c8.se>
2026-03-02 13:47:31 +0100

    autocommit complete 1347 (31)

--- ITINF25/lektion6/exception/exc2.py
+++ ITINF25/lektion6/exception/exc2.py
@@ -2,6 +2,7 @@ def delat_med_ett(divisor):
     try:
         result = 1 / divisor
         return result
+    # Vi fånger vilket undantag som helst, och kan komma åt dess info som variabeln e
     except Exception as e:
         print("DEBUG OUTPUT", e)
         return 0
