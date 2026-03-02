166d41b0b3ecb33adce8eb851c36a057a1160ecf
Kent Larsson <kent@c8.se>
2026-03-02 14:13:51 +0100

    autocommit partial 1413 (51)

--- ITINF25/lektion6/exception/custom_caller copy.py
+++ ITINF25/lektion6/exception/custom_caller copy.py
@@ -5,7 +5,4 @@ try:
     ulla = BankAccount("Ulla")
     ulla.deposit_to_account(amount)
 except ValueError as e:
-    if "negative" in str(e):
-        print("Du skrev ett negativt tal!")
-    else:
-        print("Nått är galet")
+    print("Du skrev ett negativt tal!")
