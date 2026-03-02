ce95b4dee4319ff9d792b8e87e747247645acfe4
Kent Larsson <kent@c8.se>
2026-03-02 10:25:48 +0100

    autocommit complete 1025 (48)

--- ITINF25/lektion6/objektorienterad1/use_oo.py
+++ ITINF25/lektion6/objektorienterad1/use_oo.py
@@ -2,13 +2,13 @@ import bankaccount
 
 p = print
 
-sven_account = bankaccount.BankAccount("Sven")
-sven_account.deposit_to_account(100)
+sven = bankaccount.BankAccount("Sven")
+sven.deposit_to_account(100)
 
-ulla_account = bankaccount.BankAccount("Ulla")
-ulla_account.deposit_to_account(10000)
+ulla = bankaccount.BankAccount("Ulla")
+ulla.deposit_to_account(10000)
 
-ulla_account.withdraw_from_account(1000)
-sven_account.deposit_to_account(1000)
+ulla.withdraw_from_account(1000)
+sven.deposit_to_account(1000)
 
-p(sven_account, ulla_account)
+p(sven, ulla)
