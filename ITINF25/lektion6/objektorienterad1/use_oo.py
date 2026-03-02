2c96d6289c91a0e70549e7c22d3ce3d25b57d324
Kent Larsson <kent@c8.se>
2026-03-02 10:16:00 +0100

    autocommit complete 1016 (00)

--- ITINF25/lektion6/objektorienterad1/use_oo.py
+++ ITINF25/lektion6/objektorienterad1/use_oo.py
@@ -3,5 +3,6 @@ import bankaccount
 p = print
 
 sven_account = bankaccount.BankAccount("Sven")
-ulla_account = bankaccount.BankAccount("Sven")
+ulla_account = bankaccount.BankAccount("Ulla")
 
+p(sven_account, ulla_account)
