328934d6a9711777d1342b4be1591e75f4bb4ab0
Kent Larsson <kent@c8.se>
2026-03-02 14:01:47 +0100

    autocommit complete 1401 (47)

---
+++ ITINF25/lektion6/exception/custom.py
@@ -0,0 +1,48 @@
+import sys
+
+
+class BankAccount:
+    # En variabler som tillhör själva klassen
+    # class variables
+    next_account_id = 1
+    accounts = []
+
+    def __init__(self, name):
+        self.name = name
+
+        BankAccount.next_account_id
+
+        self.id = BankAccount.next_account_id
+        BankAccount.next_account_id += 1
+        self.balance = 0
+        BankAccount.accounts.append(self)
+
+    # Exempel på en magisk metod. Just denna gör att objektet skrivs ut
+    # som en sträng på ett fint sätt för användaren.
+    def __str__(self):
+        return f"[ BankAccount | Id: {self.id}, Name: {self.name}, Balance: {self.balance} ]"
+
+    def deposit_to_account(self, amount):
+        if amount < 0:
+            print(
+                f"Error: Cannot deposit a negative amount to {self.id}",
+                file=sys.stderr,
+            )
+            sys.exit(2)
+        self.balance += amount
+
+    def withdraw_from_account(self, amount):
+        if amount < 0:
+            print(
+                f"Error: Cannot withdraw a negative amount from {self.id}",
+                file=sys.stderr,
+            )
+            sys.exit(2)
+        self.balance -= amount
+
+    # Dekorator (eng Decorator) för att markera en metod som är
+    # klassgemensam. Passar ihop med klassvariabler.
+    @classmethod
+    def list(cls):
+        # I detta fall så är cls = BankAccount
+        return cls.accounts.copy()
