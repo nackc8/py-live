dd6a170e9640f64f5b76e653fa848a1a9663b9b6
Kent Larsson <kent@c8.se>
2026-03-02 10:21:32 +0100

    autocommit complete 1021 (32)

--- ITINF25/lektion6/objektorienterad1/bankaccount.py
+++ ITINF25/lektion6/objektorienterad1/bankaccount.py
@@ -26,49 +26,19 @@ class BankAccount:
             sys.exit(2)
         self.balance += amount
 
-
-def list_accounts():
-    return bankaccounts.copy()
-
-
-def get_account(account_id):
-    for account in bankaccounts:
-        if account[0] == account_id:
-            return account.copy()
-    else:
-        # "else" körs om ingen break har körts
-
-        # Bash-motsvarighet: echo "Error: Cannot..." >&2
-        print(
-            f"Error: Cannot find account with id {account_id}",
-            file=sys.stderr,
-        )
-        # Bash-motsvarighet: exit 2
-        sys.exit(2)
+    def withdraw_from_account(self, amount):
+        if amount < 0:
+            print(
+                f"Error: Cannot withdraw a negative amount from {self.id}",
+                file=sys.stderr,
+            )
+            sys.exit(2)
+        self.balance -= amount
 
 
-def withdraw_from_account(account_id, amount):
-    if amount < 0:
-        print(
-            f"Error: Cannot withdraw a negative amount from {account_id}",
-            file=sys.stderr,
-        )
-        sys.exit(2)
-
-    for account in bankaccounts:
-        if account[0] == account_id:
-            account[2] -= amount
-            break
-    else:
-        # "else" körs om ingen break har körts
-
-        # Bash-motsvarighet: echo "Error: Cannot..." >&2
-        print(
-            f"Error: Cannot find account with id {account_id} for withdrawal",
-            file=sys.stderr,
-        )
-        # Bash-motsvarighet: exit 2
-        sys.exit(2)
+# Senare!
+# def list_accounts():
+#     return bankaccounts.copy()
 
 
 # Mål
