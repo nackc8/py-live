adadda619d17c023a1e18a0e04e9c63aefea5084
Kent Larsson <kent@c8.se>
2026-03-02 14:07:58 +0100

    autocommit complete 1407 (58)

--- ITINF25/lektion6/exception/custom_pre_caller.py
+++ ITINF25/lektion6/exception/custom_pre_caller.py
@@ -1,7 +1,8 @@
 from custom_pre import BankAccount
 
 try:
-amount = int(input("How much do you deposit to Ulla? "))
-
-ulla = BankAccount("Ulla")
-ulla.deposit_to_account(amount)
+    amount = int(input("How much do you deposit to Ulla? "))
+    ulla = BankAccount("Ulla")
+    ulla.deposit_to_account(amount)
+except ValueError as e:
+    print("Du skrev ett negativt tal!")
