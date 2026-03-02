edc01a1c9c904c57fd03dcb61f6e103b639e5d05
Kent Larsson <kent@c8.se>
2026-03-02 09:12:55 +0100

    autocommit complete 0912 (55)

--- ITINF25/lektion6/imperativ/bankaccount.py
+++ ITINF25/lektion6/imperativ/bankaccount.py
@@ -1,12 +1,15 @@
 bankaccounts = []
 
+
 def create_account(account_id, name):
-    # Ett konto = en tupel (account_id, name, )
+    # Ett konto = en tupel (account_id: int, name: str, balance: int)
+    account = (account_id, name, 0)
+
 
 # Mål
 # - Lagra ett kontoid
 # - Lagra ett namn
-# - Lagra saldo
+# - Lagra saldo (balance)
 # - Uttag (withdraw)
 # - Insättning (deposit)
 # - Lägg till ett konto
