7d731f55a5e3a01e8d17cb584a889d5ab30c2768
Kent Larsson <kent@c8.se>
2026-03-02 13:55:50 +0100

    autocommit complete 1355 (50)

--- ITINF25/lektion6/exception/important.py
+++ ITINF25/lektion6/exception/important.py
@@ -1,2 +1,11 @@
 # TypeError: https://docs.python.org/3/library/exceptions.html#TypeError
 # Kort sammanfattning: Fel typ
+#
+# ValueError: https://docs.python.org/3/library/exceptions.html#ValueError
+# Kort sammanfattning: Rätt typ, men fel värde ändå
+
+
+def greet(full_name):
+    parts = full_name.split()
+    if (len(parts) < 2):
+        
