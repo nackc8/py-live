a3e821c25bd8a2edb98a7b286af06a9c077be7c4
Kent Larsson <kent@c8.se>
2026-03-02 15:07:14 +0100

    autocommit complete 1507 (14)

--- ITINF25/lektion6/abstract/abstract1.py
+++ ITINF25/lektion6/abstract/abstract1.py
@@ -4,4 +4,10 @@ class MiniGendoc:
         self.parts = []
 
     def add_header(self, header):
-        self.parts.append(header)
+        self.parts.append(("HEADER", header))
+
+    def add_paragraph(self, paragraph):
+        self.parts.append(("PARAGRAPH", paragraph))
+
+    def __str__(self):
+        return [part for part in self.parts]
