83eb32e98ee2a776b15a374964a73c90ebec1e29
Kent Larsson <kent@c8.se>
2026-03-02 13:51:39 +0100

    autocommit complete 1351 (39)

--- ITINF25/lektion6/exception/exc3.py
+++ ITINF25/lektion6/exception/exc3.py
@@ -7,5 +7,9 @@ def delat_med_ett(divisor):
         return 0
 
 
+# Undantaget "bubblar upp" då det inte fångas, vi
+# fångar bara ZeroDivisonError. Men i vårt fall är
+# undantaget:
+# TypeError: unsupported operand type(s) for /: 'int' and 'str'
 res = delat_med_ett("hej")
 print(res)
