8902c2c44c731f3c822ebf743fcdb9d3a2a799ec
Kent Larsson <kent@c8.se>
2026-03-02 10:27:30 +0100

    autocommit partial 1027 (30)

---
+++ ITINF25/lektion6/objektorienterad1 copy/bankaccount.py
@@ -0,0 +1,54 @@
+import sys
+
+bankaccounts = []
+
+next_account_id = 1
+
+
+class BankAccount:
+    def __init__(self, name):
+        # Notera att self.name och name är olika variabler
+        self.name = name
+        global next_account_id
+        self.id = next_account_id
+        next_account_id += 1
+        self.balance = 0
+
+    # Exempel på en magisk metod. Just denna gör att objektet skrivs ut
+    # som en sträng på ett fint sätt för användaren.
+    def __str__(self):
+        return f"[ BankAccount | Id: {self.id}, Name: {self.name}, Balance: {self.balance} ]"
+
+    def deposit_to_account(self, amount):
+        if amount < 0:
+            print(
+                f"Error: Cannot deposit a negative amount to {self.id}",
+                file=sys.stderr,
+            )
+            sys.exit(2)
+        self.balance += amount
+
+    def withdraw_from_account(self, amount):
+        if amount < 0:
+            print(
+                f"Error: Cannot withdraw a negative amount from {self.id}",
+                file=sys.stderr,
+            )
+            sys.exit(2)
+        self.balance -= amount
+
+
+# Senare!
+# def list_accounts():
+#     return bankaccounts.copy()
+
+
+# Mål
+# - Lagra ett kontoid ✔
+# - Lagra ett namn ✔
+# - Lagra saldo (balance) ✔
+# - Insättning (deposit) ✔
+# - Uttag (withdraw) ✔
+# - Lägg till ett konto ✔
+# - Lista konton  ✔
+# - Hitta konto  ✔
