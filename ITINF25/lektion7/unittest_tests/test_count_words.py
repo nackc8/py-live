04cca69f2f9e6436daf51343b223f8e56ff300bb
Kent Larsson <kent@c8.se>
2026-03-03 12:45:30 +0100

    autocommit complete 1245 (30)

--- ITINF25/lektion7/unittest_tests/test_count_words.py
+++ ITINF25/lektion7/unittest_tests/test_count_words.py
@@ -8,28 +8,15 @@ class TestStringMethods(unittest.TestCase):
         result = count_words("hej hej hej hej hej hej hej hej hej hej")
         self.assertEqual(result, {"hej": 10})
 
+    def test_hopp_5(self):
+        assert count_words("hopp hopp hopp hopp hopp") == {"hopp": 5}
 
-def test_hopp_5():
-    assert count_words("hopp hopp hopp hopp hopp") == {"hopp": 5}
+    def test_hej_hopp_3(self):
+        assert count_words("hej hej hej hopp hopp hopp") == {"hej": 3, "hopp": 3}
 
+    def test_not_hopp_2(self):
+        assert count_words("hopp hopp hopp") != {"hopp": 2}
 
-def test_hej_hopp_3():
-    assert count_words("hej hej hej hopp hopp hopp") == {"hej": 3, "hopp": 3}
-
-
-def test_not_hopp_2():
-    assert count_words("hopp hopp hopp") != {"hopp": 2}
-
-
-# "Eget" sätt att säkerställa att ett undantag kastas
-def test_not_string_diy():
-    try:
-        count_words(12345)
-        assert False
-    except Exception:
-        assert True
-
-
-def test_not_string_lean_into_pytest():
-    with pytest.raises(Exception):
-        count_words(12345)
+    def test_not_string(self):
+        with pytest.raises(Exception):
+            count_words(12345)
