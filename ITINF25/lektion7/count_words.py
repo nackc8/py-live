3e3750ef1f03f2a1d72baa547157515f06225562
Kent Larsson <kent@c8.se>
2026-03-03 09:28:18 +0100

    autocommit complete 0928 (18)

--- ITINF25/lektion7/count_words.py
+++ ITINF25/lektion7/count_words.py
@@ -1,6 +1,6 @@
 # 10: Red - Skriv ett test som inte lyckas... ännu
 # 20: Green - Få alla test hittils att passera, uppmuntrat att fuska
-# 30: Refactor
+# 30: Refactor - Snygga till koden vid behov, inget mer test ska uppfyllas men alla ska fortsätta att passera
 # 40: GOTO 10
 
 
