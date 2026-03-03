5ad2822608e05ef39624d9038395eaef05cefaa0
Kent Larsson <kent@c8.se>
2026-03-03 12:56:35 +0100

    autocommit complete 1256 (35)

--- ITINF25/lektion7/doctests/count_words.py
+++ ITINF25/lektion7/doctests/count_words.py
@@ -1,7 +1,7 @@
-# 10: Red - Skriv ett test som inte lyckas... ännu
-# 20: Green - Få alla test hittils att passera, uppmuntrat att fuska
-# 30: Refactor - Snygga till koden vid behov, inget mer test ska uppfyllas men alla ska fortsätta att passera
-# 40: GOTO 10
+"""
+Counts occurances of words in a string and returns a
+dictionary with word -> occurance key values.
+"""
 
 
 def count_words(text_string):
