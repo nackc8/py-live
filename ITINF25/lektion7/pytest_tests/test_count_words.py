0ced5a8aecc9f9fb8c0683cd44aceaea4841ec29
Kent Larsson <kent@c8.se>
2026-03-03 11:08:26 +0100

    autocommit complete 1108 (26)

--- ITINF25/lektion7/pytest_tests/test_count_words.py
+++ ITINF25/lektion7/pytest_tests/test_count_words.py
@@ -11,3 +11,7 @@ def test_hopp_5():
 
 def test_hej_hopp_3():
     assert count_words("hej hej hej hopp hopp hopp") == {"hej": 3, "hopp": 3}
+
+
+def test_not_hopp_2():
+    assert count_words("hopp hopp hopp") != {"hopp": 2}
