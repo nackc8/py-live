5b38fdf135c89a8b7f11a71b07ee05a5ad9dacb3
Kent Larsson <kent@c8.se>
2026-03-03 12:49:19 +0100

    autocommit complete 1249 (19)

--- ITINF25/lektion7/unittest_tests/test_count_words.py
+++ ITINF25/lektion7/unittest_tests/test_count_words.py
@@ -23,3 +23,7 @@ class TestStringMethods(unittest.TestCase):
     def test_not_string(self):
         with self.assertRaises(Exception):
             count_words(12345)
+
+
+if __name__ == "__main__":
+    unittest.main()
