2430e0205e8ed1f0dbd36425439c8e050a29f1e2
Kent Larsson <kent@c8.se>
2026-03-03 12:40:14 +0100

    autocommit complete 1240 (14)

---
+++ ITINF25/lektion7/unittest_tests/test_count_words.py
@@ -0,0 +1,32 @@
+import pytest
+from count_words import count_words
+
+
+def test_hej_10():
+    assert count_words("hej hej hej hej hej hej hej hej hej hej") == {"hej": 10}
+
+
+def test_hopp_5():
+    assert count_words("hopp hopp hopp hopp hopp") == {"hopp": 5}
+
+
+def test_hej_hopp_3():
+    assert count_words("hej hej hej hopp hopp hopp") == {"hej": 3, "hopp": 3}
+
+
+def test_not_hopp_2():
+    assert count_words("hopp hopp hopp") != {"hopp": 2}
+
+
+# "Eget" sätt att säkerställa att ett undantag kastas
+def test_not_string_diy():
+    try:
+        count_words(12345)
+        assert False
+    except Exception:
+        assert True
+
+
+def test_not_string_lean_into_pytest():
+    with pytest.raises(Exception):
+        count_words(12345)
