b348ce5cbf666d65a9131a9122ee5849dd7b2c1f
Kent Larsson <kent@c8.se>
2026-03-03 12:47:58 +0100

    autocommit complete 1247 (58)

--- ITINF25/lektion7/unittest_tests/test_count_words.py
+++ ITINF25/lektion7/unittest_tests/test_count_words.py
@@ -10,13 +10,15 @@ class TestStringMethods(unittest.TestCase):
 
     def test_hopp_5(self):
         result = count_words("hopp hopp hopp hopp hopp")
-        self.assert
+        self.assertEqual(result, {"hopp": 5})
 
     def test_hej_hopp_3(self):
-        result = count_words("hej hej hej hopp hopp hopp") == {"hej": 3, "hopp": 3}
+        result = count_words("hej hej hej hopp hopp hopp")
+        self.assertEqual(result, {"hej": 3, "hopp": 3})
 
     def test_not_hopp_2(self):
-        result = count_words("hopp hopp hopp") != {"hopp": 2}
+        result = count_words("hopp hopp hopp")
+        self.assertEqual(result, {"hopp": 2})
 
     def test_not_string(self):
         with pytest.raises(Exception):
