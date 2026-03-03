d196bc7d1ff8f332719b3986ec0d412046f15325
Kent Larsson <kent@c8.se>
2026-03-03 09:22:22 +0100

    autocommit complete 0922 (22)

--- ITINF25/lektion7/test_count_words.py
+++ ITINF25/lektion7/test_count_words.py
@@ -1,4 +1,6 @@
 from count_words import count_words
 
+print("Test: Hej 10 gånger: ", end="")
 if count_words("hej hej hej hej hej hej hej hej hej hej") != {"hej": 10}:
     raise AssertionError("Hej ska upprepas 10 gånger")
+else:
