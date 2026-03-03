7f09ff35398d25094a0d928a284cd41522cc22bd
Kent Larsson <kent@c8.se>
2026-03-03 13:10:50 +0100

    autocommit complete 1310 (50)

--- ITINF25/lektion7/doctests/string_analytics.py
+++ ITINF25/lektion7/doctests/string_analytics.py
@@ -28,6 +28,7 @@ def count_words(text_string):
     >>> count_words(12345)
     Traceback (most recent call last):
         ...
+    AttributeError: 'int' object has no attribute 'split'
     """
     words = text_string.split()
     count = dict()
