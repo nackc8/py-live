802cf54edb7535b9f8e95a3f8117870947ab914f
Kent Larsson <kent@c8.se>
2026-03-03 13:03:16 +0100

    autocommit complete 1303 (16)

--- ITINF25/lektion7/doctests/string_analytics.py
+++ ITINF25/lektion7/doctests/string_analytics.py
@@ -18,8 +18,10 @@ def count_words(text_string):
     >>> count_words("hopp hopp hopp hopp hopp")
     {"hopp": 5}
 
-            result = count_words("hopp hopp hopp")
-        self.assertNotEqual(result, {"hopp": 2})
+    It cannot cound three "hopp" wrong:
+
+    >>> count_words("hopp hopp hopp")
+    {"hopp": 2}
 
 
 
