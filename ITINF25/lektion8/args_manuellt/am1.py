6491e8bb6c691066e780e8fb71a59d33e131f421
Kent Larsson <kent@c8.se>
2026-03-05 09:19:11 +0100

    autocommit complete 0919 (11)

--- ITINF25/lektion8/args_manuellt/am1.py
+++ ITINF25/lektion8/args_manuellt/am1.py
@@ -2,10 +2,13 @@ import sys
 
 if len(sys.argv) != 3:
     print(
-        "Please add exactly three arguments to the script: ",
+        "Please add exactly three arguments to the script",
+        # Utmatning till stderr
         file=sys.stderr,
+        # Mata ut det direkt
         flush=True,
     )
+    sys.exit(1)
 
 print("arg 0: " + sys.argv[0])
 print("arg 1: " + sys.argv[1])
