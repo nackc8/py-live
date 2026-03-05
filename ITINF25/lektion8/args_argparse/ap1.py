b6407cb9dc852b00597459a73f394226c4fc5c24
Kent Larsson <kent@c8.se>
2026-03-05 12:44:15 +0100

    autocommit complete 1244 (15)

--- ITINF25/lektion8/args_argparse/ap1.py
+++ ITINF25/lektion8/args_argparse/ap1.py
@@ -32,7 +32,7 @@ parser.add_argument(
 
 parser.add_argument("filename")
 parser.add_argument("string1")
-parser.add_argument("string2")
+parser.add_argument("string2", )
 
 parsed_args = parser.parse_args(sys.argv[1:])
 
