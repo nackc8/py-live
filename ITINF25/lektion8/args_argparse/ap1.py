427d7bd375e605beba130fdba9f51c89852dd952
Kent Larsson <kent@c8.se>
2026-03-05 10:40:36 +0100

    autocommit complete 1040 (36)

--- ITINF25/lektion8/args_argparse/ap1.py
+++ ITINF25/lektion8/args_argparse/ap1.py
@@ -15,7 +15,7 @@ parser = argparse.ArgumentParser(
 #               delete characters in ARRAY1, do not translate
 
 
-parser.add_argument("-c", "-C", "--complement")
+parser.add_argument("-c", "-C", "--complement", action="store_true")
 
 parser.parse_args(sys.argv[1:])
 
