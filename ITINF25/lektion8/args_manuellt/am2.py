3acf3261670ea1da1d287b79890e1037d7e8b419
Kent Larsson <kent@c8.se>
2026-03-05 09:23:59 +0100

    autocommit complete 0923 (59)

--- ITINF25/lektion8/args_manuellt/am2.py
+++ ITINF25/lektion8/args_manuellt/am2.py
@@ -13,17 +13,5 @@ import sys
 #        -d, --delete
 #               delete characters in ARRAY1, do not translate
 
-
-if len(sys.argv) != 3:
-    print(
-        "Please add exactly three arguments to the script",
-        # Utmatning till stderr
-        file=sys.stderr,
-        # Mata ut det direkt
-        flush=True,
-    )
-    sys.exit(1)
-
-print("arg 0: " + sys.argv[0])
-print("arg 1: " + sys.argv[1])
-print("arg 2: " + sys.argv[2])
+for arg in sys.argv:
+    print(arg)
