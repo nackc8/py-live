4cd11c9439943681c9aeff9568f0b910c79fefa6
Kent Larsson <kent@c8.se>
2026-03-05 10:37:03 +0100

    autocommit complete 1037 (03)

--- ITINF25/lektion8/args_argparse/ap1.py
+++ ITINF25/lektion8/args_argparse/ap1.py
@@ -1,6 +1,8 @@
 import argparse
 import sys
 
+# Se am1.py för vad vi översätter till argparse
+
 parser = argparse.ArgumentParser(
     prog="ProgramName",
     description="What the program does",
