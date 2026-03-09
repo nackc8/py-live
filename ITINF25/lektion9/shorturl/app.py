82e9b4e9d803a40f1d8a1c8a79e35249529558db
Kent Larsson <kent@c8.se>
2026-03-09 10:32:13 +0100

    autocommit complete 1032 (13)

--- ITINF25/lektion9/shorturl/app.py
+++ ITINF25/lektion9/shorturl/app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route("/", methods=["POST"])
 def add():
     return "<p>Ta emot en URL, skapa och lagra en kort url, skicka tillbaka den korta urlen</p>"
+
+
+@app.route("/gh6", methods=["DELETE"])
+def remove():
+    return "<p>Radera den korta URL:en <url>"
