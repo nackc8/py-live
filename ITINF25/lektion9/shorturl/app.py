1c48bc82f80498694880f18d3ff69055674d9704
Kent Larsson <kent@c8.se>
2026-03-09 14:15:44 +0100

    autocommit complete 1415 (44)

--- ITINF25/lektion9/shorturl/app.py
+++ ITINF25/lektion9/shorturl/app.py
@@ -12,6 +12,7 @@ def add():
     original_url = json["url"]
 
     url_map = db.Url(original=original_url)
+    url_map.save()
     url_map.short = url_map.get_id()
     print(url_map.short)
     url_map.save()
