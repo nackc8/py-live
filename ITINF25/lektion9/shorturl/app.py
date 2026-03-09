58187421dc6b9f910bcad4f04e1efbfdde319ce5
Kent Larsson <kent@c8.se>
2026-03-09 10:50:02 +0100

    autocommit complete 1050 (02)

--- ITINF25/lektion9/shorturl/app.py
+++ ITINF25/lektion9/shorturl/app.py
@@ -1,10 +1,12 @@
-from flask import Flask, Request
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["POST"])
 def add():
+    test = request.data.get_json()
+    print(test)
     return "<p>Ta emot en URL, skapa och lagra en kort url, skicka tillbaka den korta urlen</p>"
 
 
