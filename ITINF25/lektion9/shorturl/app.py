42069c7a92ad4381bfb8e1426d62b1f7be163975
Kent Larsson <kent@c8.se>
2026-03-09 10:37:36 +0100

    autocommit complete 1037 (36)

--- ITINF25/lektion9/shorturl/app.py
+++ ITINF25/lektion9/shorturl/app.py
@@ -8,11 +8,6 @@ def add():
     return "<p>Ta emot en URL, skapa och lagra en kort url, skicka tillbaka den korta urlen</p>"
 
 
-@app.route("/gh6", methods=["DELETE"])
-def remove():
-    return "<p>Radera den korta URL:en <url>"
-
-
-@app.route("/user/<username>")
-def profile(username):
-    return f"{username}'s profile"
+@app.route("/<shorturl>", methods=["DELETE"])
+def remove(shorturl):
+    return f"<p>Radera den korta URL:en {shorturl}"
